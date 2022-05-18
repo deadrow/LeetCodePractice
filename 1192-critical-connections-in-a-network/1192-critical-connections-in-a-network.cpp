2d25565b7d7fc3084d1e2ab809ac31509baf4edb
deadrow <ankursingh.nit5@gmail.com>
2022-05-18 21:53:10 +0530

    Time: 1259 ms (13.81%), Space: 193.3 MB (27.52%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> disc, low;
     vector<vector<int>> result;
     
-    void dfs(int idx, int parent, vector<vector<int>> &edges, int &time)
+    void dfs(int idx, int parent, vector<vector<int>> &edges, int time)
     {
-        if(disc[idx] > 0)
+        if(disc[idx] >= 0)
             return;
         
         disc[idx] = time++;
@@ -27,35 +27,51 @@ public:
     
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
 
-        // build adjacency list
-        vector<vector<int>> adj(n);
-        for(auto c: connections)
+        vector<vector<int>> edges(n);
+        for(auto it : connections)
         {
-            adj[c[0]].push_back(c[1]);
-            adj[c[1]].push_back(c[0]);
+            edges[it[0]].push_back(it[1]);
+            edges[it[1]].push_back(it[0]);
         }
-        int t=0;
-        vector<int> id(n,-1), low(n,-1);
-        vector<vector<int>> critical;
-        function<void(int,int)> dfs_targen = [&](int v,int parent)
-        {
-            if(id[v]>=0)
-                return;
-            id[v] = t++;
-            low[v] = id[v];
-            for(auto n: adj[v])
-            {
-                if(n==parent)
-                    continue;
-                dfs_targen(n,v);
-                
-                low[v] = min(low[v],low[n]);
-                if(id[v] < low[n])     // important as child was never able to reach back i.e. new SSC and count it ( no back link )
-                    critical.push_back({v,n});
-            }
-        };
 
-        dfs_targen(0,-1);
-        return critical;
+        disc.resize(n, -1);
+        low.resize(n, -1);
+
+        int time = 0;
+        
+        dfs(0, -1, edges, time);
+        
+        return result;
+        
+//         // build adjacency list
+//         vector<vector<int>> adj(n);
+//         for(auto c: connections)
+//         {
+//             adj[c[0]].push_back(c[1]);
+//             adj[c[1]].push_back(c[0]);
+//         }
+//         int t=0;
+//         vector<int> id(n,-1), low(n,-1);
+//         vector<vector<int>> critical;
+//         function<void(int,int)> dfs_targen = [&](int v,int parent)
+//         {
+//             if(id[v]>=0)
+//                 return;
+//             id[v] = t++;
+//             low[v] = id[v];
+//             for(auto n: adj[v])
+//             {
+//                 if(n==parent)
+//                     continue;
+//                 dfs_targen(n,v);
+                
+//                 low[v] = min(low[v],low[n]);
+//                 if(id[v] < low[n])     // important as child was never able to reach back i.e. new SSC and count it ( no back link )
+//                     critical.push_back({v,n});
+//             }
+//         };
+
+//         dfs_targen(0,-1);
+//         return critical;
     }
 };
