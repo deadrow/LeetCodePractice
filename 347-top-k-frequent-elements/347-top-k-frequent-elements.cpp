19d6626af0d95e397794f5b0a00097812df1165d
deadrow <ankursingh.nit5@gmail.com>
2022-05-21 17:08:11 +0530

    Time: 24 ms (42.12%), Space: 13.7 MB (42.75%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
+    unordered_map<int,int> cache;
+    int quickselect(vector<int>& unique, int low, int high, int k)
+    {
+        int pivot = cache[unique[high]];
+        int left = low;
+        
+        for(int i=low;i<high;i++)
+        {
+            if(cache[unique[i]] <= pivot)
+            {
+                swap(unique[left], unique[i]);
+                left++;
+            }
+        }
+        
+        swap(unique[left], unique[high]);
+        
+        if(k < left) // meaning in 1st half
+            return quickselect(unique, low, left-1, k);
+        else if(k > left)
+            return quickselect(unique, left+1, high, k);
+        return unique[left];
+    }
+    
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        unordered_map<int,int> cache;
-        
         for(auto it : nums)
             cache[it]++;
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> min_heap;
-        
+        vector<int> unique;
         for(auto it : cache)
         {
-            min_heap.push({it.second, it.first});
-            
-            if(min_heap.size() > k)
-                min_heap.pop();
+            unique.push_back(it.first);
         }
+        
+        int low = 0;
+        int high = unique.size()-1;
+        quickselect(unique, low, high, unique.size()-k);
         
         vector<int> res;
-        while(!min_heap.empty())
-        {
-            res.push_back(min_heap.top().second);
-            min_heap.pop();
-        }
-        
+        for(int i=unique.size()-k;i<unique.size();i++)
+            res.push_back(unique[i]);
         return res;
     }
 };
