f1f3ac4d9827056233396040a9466539a1716fda
deadrow <ankursingh.nit5@gmail.com>
2022-05-24 22:44:07 +0530

    Time: 3 ms (91.63%), Space: 10.6 MB (13.68%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> memo;
+    int dp(vector<int>& stones, int i, int curSum, int totalSum)
+    {
+        if(i < 0)
+            return abs(totalSum - 2* curSum);
+        
+        if(memo[i][curSum] != -1)
+            return memo[i][curSum];
+        
+        int take = dp(stones, i-1, curSum + stones[i], totalSum);
+        int leave = dp(stones, i-1, curSum, totalSum);
+        
+        return memo[i][curSum] = min(take, leave);
+    }
+    int lastStoneWeightII(vector<int>& stones) {
+        int totalSum = accumulate(stones.begin(), stones.end(), 0);
+        
+        memo.resize(stones.size()+1, vector<int>(totalSum+1, -1));
+        
+        return dp(stones, stones.size()-1, 0, totalSum);
+    }
+};
