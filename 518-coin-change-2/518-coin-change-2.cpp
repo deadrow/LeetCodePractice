cfb46fa7832da406856de4d5e0d40578f6b16b44
deadrow <ankursingh.nit5@gmail.com>
2022-05-24 11:53:50 +0530

    Time: 17 ms (73.33%), Space: 19 MB (15.12%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -25,20 +25,24 @@ public:
     int change(int amount, vector<int>& coins)
     {
         memo.resize(coins.size()+1, vector<int>(amount+1,-1));
-        
+
+        // Unbounded knapsack solution
         return dp(coins, coins.size()-1, amount);
-//         vector<int>dp(amount+1);
-//         dp[0] = 1;
         
-//         for(auto coin : coins)
-//         {
-//             for(int i=0;i<=amount;i++)
-//             {
-//                 if(i-coin >= 0)
-//                     dp[i] += dp[i-coin];
-//             }
-//         }
+        /*
+        vector<int>dp(amount+1);
+        dp[0] = 1;
         
-//         return dp[amount];
+        for(auto coin : coins)
+        {
+            for(int i=0;i<=amount;i++)
+            {
+                if(i-coin >= 0)
+                    dp[i] += dp[i-coin];
+            }
+        }
+        
+        return dp[amount];
+        */
     }
 };
