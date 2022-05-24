10bd016b33334994d43e20b6b02b9877e3f6a725
deadrow <ankursingh.nit5@gmail.com>
2022-05-24 11:52:08 +0530

    Time: 27 ms (56.36%), Space: 19 MB (17.01%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>>memo;
+    int dp(vector<int>& coins, int i, int amount)
+    {
+        if(amount == 0)
+            return 1;
+        
+        if(i < 0)
+            return 0;
+        
+        if(memo[i][amount] != -1)
+            return memo[i][amount];
+        
+        int take = 0;
+        if(coins[i] <= amount)
+        {
+            take = dp(coins, i, amount - coins[i]);
+        }
+        
+        int leave = dp(coins, i-1, amount);
+        return memo[i][amount] = take+leave;
+    }
+    
+    int change(int amount, vector<int>& coins)
+    {
+        memo.resize(coins.size()+1, vector<int>(amount+1,-1));
+        
+        return dp(coins, coins.size()-1, amount);
+//         vector<int>dp(amount+1);
+//         dp[0] = 1;
+        
+//         for(auto coin : coins)
+//         {
+//             for(int i=0;i<=amount;i++)
+//             {
+//                 if(i-coin >= 0)
+//                     dp[i] += dp[i-coin];
+//             }
+//         }
+        
+//         return dp[amount];
+    }
+};
