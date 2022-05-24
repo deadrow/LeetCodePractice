8b8187c236ae2c9fa37dde324adbc63e182a8f8e
deadrow <ankursingh.nit5@gmail.com>
2022-05-24 22:13:15 +0530

    Time: 221 ms (14.61%), Space: 49.3 MB (10.23%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,8 +1,38 @@
 class Solution {
 public:
+    vector<vector<int>>memo;
+    int dp(vector<int>& coins, int i, int amount)
+    {
+        if(amount == 0)
+            return 0;
+        
+        if(i < 0 || amount < 0)
+            return INT_MAX;
+        
+        if(memo[i][amount] != -1)
+            return memo[i][amount];
+        
+        int take = INT_MAX;
+        if(coins[i] <= amount)
+        {
+            int res = dp(coins, i, amount - coins[i]);
+            take = res != INT_MAX ? res+1 : take;
+        }
+
+        int leave = dp(coins, i-1, amount);
+        return memo[i][amount] = min(take,leave);
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
         if(amount < 1) return 0;
-                    
+        
+        memo.resize(coins.size()+1, vector<int>(amount+1,-1));
+        
+        // Unbounded knapsack solution
+        int ret = dp(coins, coins.size()-1, amount);
+        return ret != INT_MAX ? ret : -1;
+        
+        /*
         vector<int> dp(amount+1, amount+1);
         
         dp[0] = 0;
@@ -16,5 +46,6 @@ public:
         }
         
         return (dp[amount] == amount+1) ? -1: dp[amount];
+        */
     }
 };
