d4993682e38325219eac0c0a0030f269ffd197aa
deadrow <ankursingh.nit5@gmail.com>
2022-05-28 18:24:02 +0530

    Time: 346 ms (9.73%), Space: 73.1 MB (23.55%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -8,13 +8,11 @@ public:
         string second = s;
         reverse(second.begin(), second.end());
         
-        for(int i=0;i<=n;i++)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=n;j++)
+            for(int j=1;j<=n;j++)
             {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else if(first[i-1] == second[j-1])
+                if(first[i-1] == second[j-1])
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
