eeea59749659bc30b127df7d6a846f46f2a4612f
deadrow <ankursingh.nit5@gmail.com>
2022-05-29 11:32:24 +0530

    Time: 79 ms (49.87%), Space: 16.3 MB (49.36%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -3,33 +3,28 @@ public:
     int maxProduct(vector<string>& words) {
         int maxProduct = 0;
         
+        vector<int> bitMasks;
+        vector<int> lengths;
+        
         for(int i=0;i<words.size();i++)
         {
-            string first = words[i];
-            vector<int>table(26);
-            for(auto it : first)
+            int bitMask = 0;
+            for(auto it : words[i])
             {
-                table[it-97]++;
+                bitMask |= (1 << (it-97));
             }
-            
+            bitMasks.push_back(bitMask);
+            lengths.push_back(words[i].length());
+        }
+        
+        
+        for(int i=0;i<words.size();i++)
+        {
             for(int j=i+1;j<words.size();j++)
             {
-                string second = words[j];
-
-                bool exists = false;
-                for(auto it : second)
+                if((bitMasks[i] & bitMasks[j]) == 0)
                 {
-                    if(table[it-97] != 0)
-                    {
-                        exists = true;
-                        break;
-                    }
-                }
-                
-                if(!exists)
-                {
-                    int cur = first.length()*second.length();
-                    maxProduct = max(maxProduct, cur);
+                    maxProduct = max(maxProduct, lengths[i]* lengths[j]);
                 }
             }
         }
