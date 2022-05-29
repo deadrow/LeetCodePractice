ceecb684c810f96c780a2ca96a28f2aadcedf1c9
deadrow <ankursingh.nit5@gmail.com>
2022-05-29 10:47:25 +0530

    Time: 573 ms (21.08%), Space: 30.1 MB (35.77%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,42 +2,53 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        
-        // expand around center solution
-        string res;
         int maxLength = 0;
+        int start = 0;
+        int end = 0;
+        
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        
+        // each letter is itself palindrome
+        // so all dp[i][i]  would be true
+        // for 2 letters we need to check if
+        // i and i+1 is equal
         
         for(int i=0;i<n;i++)
+            dp[i][i] = true;
+        
+        // for length =2 like "aa"
+        for(int i=0;i<n-1;i++)
         {
-            // odd case 'aba'
-            int left = i;
-            int right = i;
-            while(left >= 0 && right < n && s[left] == s[right])
+            if(s[i] == s[i+1])
             {
-                if(right-left+1 > maxLength)
-                {
-                    maxLength = right-left+1;
-                    res = s.substr(left, maxLength);
-                }
-                left--;
-                right++;
+                dp[i][i+1] = true;
+                maxLength = 2;
+                start = i;
+                end = i+1;
             }
-            
-            // even case like 'aa'
-            left = i;
-            right = i+1;
-            while(left >= 0 && right < n && s[left] == s[right])
+        }
+                
+        // for length > 2
+        // set i = 0 and j to offset at 2
+        for(int len=2;len<=n;len++)
+        {
+            for(int i=0;i<n-len+1;i++)
             {
-                if(right-left+1 > maxLength)
+                int j = i+len-1;
+                if(s[i] == s[j] && dp[i+1][j-1])
                 {
-                    maxLength = right-left+1;
-                    res = s.substr(left, maxLength);
+                    dp[i][j] = true;
+                    if(j-i+1 > maxLength)
+                    {
+                        maxLength = j-i+1;
+                        start = i;
+                        end = j;
+                    }
                 }
-                left--;
-                right++;
+                    
             }
         }
         
-        return res;
+        return s.substr(start, end-start+1);
     }
 };
