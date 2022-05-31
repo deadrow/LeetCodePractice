a7cbe366d49ce1b0bc097a555d45fe39b0417a54
deadrow <ankursingh.nit5@gmail.com>
2022-05-31 23:10:40 +0530

    Time: 94 ms (17.90%), Space: 28.1 MB (26.09%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.cpp
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.cpp
@@ -1,26 +1,33 @@
 class Codec {
 public:
-    string delim ="$$#$$";
-    
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
         string res;
         for(auto it : strs)
-            res += it + delim;
+        {
+            res += it.size();
+            res += it;
+        }
+            
         return res;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
         vector<string> res;
-        size_t pos = 0;
-        std::string token;
-        while ((pos = s.find(delim)) != std::string::npos) {
-            token = s.substr(0, pos);
-            res.push_back(token);
-            s.erase(0, pos + delim.length());
-        }
         
+        unsigned idx = 0;
+        while(idx < s.length())
+        {
+            uint8_t curSize = s[idx++];
+            string ans;
+            while(curSize--)
+            {
+                ans += s[idx++];
+            }
+            
+            res.push_back(ans);
+        }
         return res;
     }
 };
