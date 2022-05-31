80bf71b3f231855af1f3117c4372eeb14065c5b8
deadrow <ankursingh.nit5@gmail.com>
2022-05-31 17:08:13 +0530

    Time: 26 ms (42.49%), Space: 11.8 MB (44.47%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cpp
+++ 252-meeting-rooms/252-meeting-rooms.cpp
@@ -4,10 +4,9 @@ public:
         int n = intervals.size();
         if(n == 0) return true;
 
+        // c++ sort function sorts by 1st and then 2nd
         sort(intervals.begin(), intervals.end());
         
-        int count = 1;
-        int i = 0;
         for(int j=0;j<n-1;j++)
         {
             if(intervals[j][1] > intervals[j+1][0])
