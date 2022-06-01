3020301d400ec72111a354a23d6a8fa770dec0f0
deadrow <ankursingh.nit5@gmail.com>
2022-06-01 10:05:02 +0530

    Time: 12 ms (69.14%), Space: 8.1 MB (58.25%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -15,15 +15,15 @@ public:
         ListNode* slow = head;
         ListNode* fast = head->next;
         
-        while(slow && fast && fast->next)
+        while(slow != fast)
         {
+            if(!fast || !fast->next)
+                return false;
+            
             slow = slow->next;
             fast = fast->next->next;
-            
-            if(slow == fast)
-                return true;
         }
         
-        return false;
+        return true;
     }
 };
