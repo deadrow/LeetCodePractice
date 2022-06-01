d4f6bc0c8bb8a549d134b66fa91d2574021fbd4e
deadrow <ankursingh.nit5@gmail.com>
2022-06-01 22:00:02 +0530

    Time: 70 ms (24.78%), Space: 20.1 MB (17.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,26 +5,19 @@ public:
         
         sort(intervals.begin(), intervals.end());
         
-        stack<vector<int>> st;
-        st.push(intervals[0]);
+        res.push_back(intervals[0]);
         for(int j=1;j<intervals.size();j++)
         {
-            if(intervals[j][0] <= st.top()[1])
+            if(intervals[j][0] <= res.back()[1])
             {
-                vector<int> top = st.top();
-                st.pop();
-                st.push({top[0], max(intervals[j][1], top[1])});
+                vector<int> top = res.back();
+                res.pop_back();
+                res.push_back({top[0], max(intervals[j][1], top[1])});
             }
             else
-                st.push(intervals[j]);
+                res.push_back(intervals[j]);
         }
         
-        while(!st.empty())
-        {
-            res.push_back(st.top());
-            st.pop();
-        }
-
         return res;
     }
 };
