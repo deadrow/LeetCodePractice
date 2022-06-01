7815adfbfaac01ae3684e80c383bd2f228e748df
deadrow <ankursingh.nit5@gmail.com>
2022-06-01 22:49:43 +0530

    Time: 11 ms (96.92%), Space: 17.1 MB (40.54%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -6,20 +6,26 @@ public:
         int i = 0;
         int n = intervals.size();
         
+        // Find all intervals less than new interval intersection
         while(i < n && newInterval[0] > intervals[i][0])
         {
             res.push_back(intervals[i++]);
         }
         
+        // check if res is empty or new interval is completely bigger
+        // in that case just add it to end
         if(res.empty() || res.back()[1] < newInterval[0])
         {
             res.push_back(newInterval);
         }
         else
         {
+            // else find out last one from result and find the 
+            // max of ending index
             res.back()[1] = max(res.back()[1], newInterval[1]);
         }
 
+        // This step is exactly same as merge intervals
         for(;i<n;i++)
         {
             if(intervals[i][0] <= res.back()[1])
