0accb0a3f5732af706e62693f6f70f2c343600fd
deadrow <ankursingh.nit5@gmail.com>
2022-06-02 17:57:25 +0530

    Time: 6 ms (71.40%), Space: 7.8 MB (88.31%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> countBits(int n) {
         vector<int> ret(n+1);
         
-        for(int i=0;i<=n;i++)
+        for(int i=1;i<=n;i++)
         {
-            bitset<32> bs(i);
-            ret[i] = bs.count();
+            ret[i] = ret[i & i-1] + 1;
         }
         
         return ret;
