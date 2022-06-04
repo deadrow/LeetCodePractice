822a29c39e365c4655f92c7e6b7a46866241bc62
deadrow <ankursingh.nit5@gmail.com>
2022-06-04 12:17:16 +0530

    Time: 26 ms (22.09%), Space: 8.2 MB (22.95%) - LeetHub

---
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        
+        if(k > s.size())
+            return s.size();
+        
+        unordered_map<char, int> cache;
+        
+        int i=0;
+        int j=0;
+        int maxLen = 0;
+        while(j < s.size())
+        {
+            cache[s[j]]++;
+            
+            if(cache.size() <= k)
+            {
+                maxLen = max(maxLen, j-i+1);
+                j++;
+            }
+            else if(cache.size() > k)
+            {
+                while(cache.size() > k)
+                {
+                    cache[s[i]]--;
+                    if(cache[s[i]] == 0)
+                        cache.erase(s[i]);
+                    
+                    i++;
+                }
+                j++;
+            }
+        }
+        
+        return maxLen;
+    }
+};
