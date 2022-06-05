efba83b33e98220f18dd01646ea17353d3223cfd
deadrow <ankursingh.nit5@gmail.com>
2022-06-05 10:36:37 +0530

    Time: 794 ms (10.40%), Space: 71.9 MB (35.22%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        
-        sort(points.begin(), points.end(), [](const auto& a, const auto& b){
+        auto compare = [](const auto& a, const auto& b){
             return sqrt(abs(a[0]*a[0]) + abs(a[1]*a[1])) < sqrt(abs(b[0]*b[0]) + abs(b[1]*b[1]));
-        });
+        };
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> pq(compare);
+        
+        // sort(points.begin(), points.end(), [](const auto& a, const auto& b){
+        //     return sqrt(abs(a[0]*a[0]) + abs(a[1]*a[1])) < sqrt(abs(b[0]*b[0]) + abs(b[1]*b[1]));
+        // });
         
         vector<vector<int>> ret;
-        for(int i=0;i<k;i++)
-            ret.push_back(points[i]);
+        for(int i=0;i<points.size();i++)
+        {
+            pq.push(points[i]);
+            
+            if(pq.size() > k)
+                pq.pop();
+        }
+        
+        while(!pq.empty())
+        {
+            ret.push_back(pq.top());
+            pq.pop();
+        }
         
         return ret;
             
