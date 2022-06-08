861a5c16884b8ee279d084c9377116bfb787cfb4
deadrow <ankursingh.nit5@gmail.com>
2022-06-08 21:37:48 +0530

    Time: 1642 ms (5.05%), Space: 663 MB (5.05%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
+    vector<vector<int>>memo;
+    
+    int lcs(string s, string t, int i, int j)
+    {
+        if(i < 0 || j < 0)
+            return 0;
+        
+        if(memo[i][j] != -1)
+            return memo[i][j];
+        
+        if(s[i] == t[j])
+            return memo[i][j] = 1+lcs(s, t, i-1, j-1);
+
+        return memo[i][j] = max(lcs(s, t, i-1, j), lcs(s, t, i, j-1));
+    }
     bool isSubsequence(string s, string t) {
-        int s_i = s.size()-1;
-        int t_i = t.size()-1;
         
-        int count = 0;
-        while(s_i >= 0 && t_i >= 0)
-        {
-            if(s[s_i] == t[t_i])
-            {
-                count++;
-                s_i--;
-                t_i--;
-            }
-            else
-                t_i--;
-        }
-        
-        return count == s.size();
+        memo.resize(s.size(), vector<int>(t.size(),-1));
+        int len = lcs(s, t, s.size()-1, t.size()-1);
+        return len == s.size();
     }
 };
