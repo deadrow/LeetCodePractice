e47137a41cef1b04a96711d4d69242cdb6c0d502
deadrow <ankursingh.nit5@gmail.com>
2022-06-11 16:33:15 +0530

    Time: 253 ms (64.56%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -7,7 +7,6 @@ public:
         if(target < 0)
             return -1;
         
-
         int i=0;
         int sum = 0;
         int maxLen = INT_MIN;
@@ -21,27 +20,7 @@ public:
             if(sum == target)
                 maxLen = max(maxLen, j-i+1);
         }
-//         while(j < n)
-//         {
-//             sum += nums[j];
-              
-//             if(sum < target)
-//                 j++;
-//             else if(sum == target)
-//             {
-//                 maxLen = max(maxLen, j-i+1);
-//                 j++;
-//             }
-//             else if(sum > target)
-//             {
-//                 while(sum > target)
-//                     sum -= nums[i++];
-//                 if(sum == target)
-//                     maxLen = max(maxLen, j-i+1);
-//                 j++;
-//             }
-//         }
-        
+
         return maxLen == INT_MIN ? -1 : n-maxLen;
     }
     
