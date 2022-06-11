650ae90ed7ef79bbc54bc168dba16775742c0a80
deadrow <ankursingh.nit5@gmail.com>
2022-06-11 11:37:03 +0530

    Time: 283 ms (33.69%), Space: 53.3 MB (53.49%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* result = nullptr;
+        if(!list1)
+            return list2;
+        if(!list2)
+            return list1;
+        
+        if(list1->val <= list2->val)
+        {
+            result = list1;
+            result->next = mergeTwoLists(list1->next, list2);
+        }
+        else
+        {
+            result = list2;
+            result->next = mergeTwoLists(list1, list2->next);
+        }
+        
+        return result;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        
+        // Find out mid and last
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+        while(fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        // Break the list in 2 halves
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+        
+        // Now merge
+        ListNode* ret1 = sortList(head);
+        ListNode* ret2 = sortList(second);
+        return mergeTwoLists(ret1, ret2);
+    }
+};
