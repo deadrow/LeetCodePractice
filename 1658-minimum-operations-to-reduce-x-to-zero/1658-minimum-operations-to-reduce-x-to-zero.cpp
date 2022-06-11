a16bc393c5e37425364e323f598257a673bfed8b
deadrow <ankursingh.nit5@gmail.com>
2022-06-11 16:32:56 +0530

    Time: 206 ms (78.93%), Space: 98.6 MB (52.79%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,37 +2,45 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        vector<int>prefix(n+1);
-
         int total = accumulate(nums.begin(), nums.end(), 0);
         int target = total-x;
         if(target < 0)
             return -1;
         
+
         int i=0;
-        int j=0;
         int sum = 0;
         int maxLen = INT_MIN;
-        while(j < n)
+        
+        for(int j=0;j<n;j++)
         {
             sum += nums[j];
-              
-            if(sum < target)
-                j++;
-            else if(sum == target)
-            {
+            while(sum > target)
+                sum -= nums[i++];
+            
+            if(sum == target)
                 maxLen = max(maxLen, j-i+1);
-                j++;
-            }
-            else if(sum > target)
-            {
-                while(sum > target)
-                    sum -= nums[i++];
-                if(sum == target)
-                    maxLen = max(maxLen, j-i+1);
-                j++;
-            }
         }
+//         while(j < n)
+//         {
+//             sum += nums[j];
+              
+//             if(sum < target)
+//                 j++;
+//             else if(sum == target)
+//             {
+//                 maxLen = max(maxLen, j-i+1);
+//                 j++;
+//             }
+//             else if(sum > target)
+//             {
+//                 while(sum > target)
+//                     sum -= nums[i++];
+//                 if(sum == target)
+//                     maxLen = max(maxLen, j-i+1);
+//                 j++;
+//             }
+//         }
         
         return maxLen == INT_MIN ? -1 : n-maxLen;
     }
