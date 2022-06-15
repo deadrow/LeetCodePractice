3d9f86f48d96455a8af5fd6dcae83bf032bd3383
deadrow <ankursingh.nit5@gmail.com>
2022-06-15 21:48:40 +0530

    Time: 137 ms (52.88%), Space: 13 MB (97.96%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int isSubsquenece(string& word1, string& word2)
+    {
+        int m = word1.size();
+        int n = word2.size();
+        if(m + 1 != n)
+            return false;
+        
+        int i=0;
+        int j=0;
+        while(i < m && j < n)
+        {
+            if(word1[i] == word2[j])
+                i++;
+            if(i == m)
+                return true;
+            j++;
+        }
+        
+        return i == m;
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        
+        sort(words.begin(), words.end(), [](const string& a, const string& b){
+            return a.size() < b.size();
+        });
+        
+        vector<int> dp(words.size(),1);
+        
+        int ans = 1;
+        for(int i=1;i<words.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(isSubsquenece(words[j], words[i]))
+                    dp[i] = max(dp[i], dp[j]+1);
+            }
+            
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+};
