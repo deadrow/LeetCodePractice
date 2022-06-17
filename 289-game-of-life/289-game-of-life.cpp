8f3cc938a3670ec12137a47559729216eb7017c6
deadrow <ankursingh.nit5@gmail.com>
2022-06-17 11:05:02 +0530

    Time: 3 ms (52.48%), Space: 6.8 MB (96.56%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -6,52 +6,42 @@ public:
         int m = board.size();
         int n = board[0].size();
         
-        vector<vector<int>> ans(m, vector<int>(n));
+        // vector<vector<int>> ans(m, vector<int>(n));
         // ans = board;
         
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j] == 0)
+                int countLives = 0;
+                for(auto it : directions)
                 {
-                    int countLives = 0;
-                    for(auto it : directions)
+                    int nRow = i + it.first;
+                    int nCol = j + it.second;
+                    if(nRow >=0 && nRow < m && nCol >=0 && nCol < n)
                     {
-                        int nRow = i + it.first;
-                        int nCol = j + it.second;
-                        if(nRow >=0 && nRow < m && nCol >=0 && nCol < n)
-                        {
-                            if(board[nRow][nCol] == 1)
-                                countLives++;
-                        }
+                        if(board[nRow][nCol] == 1 || board[nRow][nCol] == -1)
+                            countLives++;
                     }
-                    
-                    if(countLives == 3)
-                        ans[i][j] = 1;
                 }
-                else
-                {
-                    int countLives = 0;
-                    for(auto it : directions)
-                    {
-                        int nRow = i + it.first;
-                        int nCol = j + it.second;
-                        if(nRow >=0 && nRow < m && nCol >=0 && nCol < n)
-                        {
-                            if(board[nRow][nCol] == 1)
-                                countLives++;
-                        }
-                    }
-                    
-                    if(countLives < 2 || countLives > 3)
-                        ans[i][j] = 0;
-                    else if(countLives == 2 or countLives == 3)
-                        ans[i][j] = 1;
-                }
+                
+                if(board[i][j] == 1 && (countLives < 2 || countLives > 3))
+                    board[i][j] = -1;
+                
+                if(board[i][j] == 0 && countLives == 3)
+                    board[i][j] = 2;
             }
         }
         
-        board = ans;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j] > 0)
+                    board[i][j] = 1;
+                else
+                    board[i][j] = 0;
+            }
+        }
     }
 };
