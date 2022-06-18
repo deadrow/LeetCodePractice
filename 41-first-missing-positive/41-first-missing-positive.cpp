b863442d327b363b17e81686bcbd02f1d68bcdc2
deadrow <ankursingh.nit5@gmail.com>
2022-06-18 22:53:40 +0530

    Time: 291 ms (31.74%), Space: 83 MB (77.58%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -5,7 +5,7 @@ public:
         while(i < nums.size())
         {
             long correct = (long)nums[i] - 1;
-            if(correct >= 0 && correct < nums.size() && nums[i] > 0 && nums[i] < nums.size() && nums[i] != nums[correct])
+            if(correct >= 0 && correct < nums.size() && nums[i] != nums[correct])
                 swap(nums[i], nums[correct]);
             else
                 i++;
