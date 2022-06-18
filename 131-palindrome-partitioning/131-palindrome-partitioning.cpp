9a56bcc4c1ab2724018c98f56af8fc327a287870
deadrow <ankursingh.nit5@gmail.com>
2022-06-18 12:14:30 +0530

    Time: 211 ms (45.14%), Space: 75.9 MB (37.10%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> ret;
+    vector<vector<bool>> memo;
     bool isPalindrome(string &s) {
         int i=0;
         int j=s.size()-1;
@@ -12,27 +13,29 @@ public:
         return true;
     }
     
-    void solve(string& s, int i, vector<string>& cur)
+    void solve(string& s, int start, vector<string>& cur)
     {
-        if(i >= s.length())
+        if(start >= s.length())
         {
             ret.push_back(cur);
             return;
         }
         
-        for(int k=i;k<s.length();k++)
+        for(int end=start;end<s.length();end++)
         {
-            string temp = s.substr(i, k-i+1);
-            if(isPalindrome(temp))
+            string temp = s.substr(start, end-start+1);
+            if(s[start] == s[end] && (end-start <= 2 || memo[start+1][end-1]))
             {
+                memo[start][end] = true;
                 cur.push_back(temp);
-                solve(s, k+1, cur);
+                solve(s, end+1, cur);
                 cur.pop_back();
             }
         }
     }
     
     vector<vector<string>> partition(string s) {
+        memo.resize(s.length(), vector<bool>(s.length()));
         vector<string> cur;
         solve(s, 0, cur);
         return ret;
