2f31818eff24afc18b86472fafda73633a05e3f4
deadrow <ankursingh.nit5@gmail.com>
2022-06-21 21:34:27 +0530

    Time: 143 ms (12.88%), Space: 61.7 MB (27.61%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> subSet;
+        subSet.push_back(nums[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            int num = nums[i];
+            if(num > subSet[subSet.size()-1])
+                subSet.push_back(num);
+            else
+            {
+                int j=0;
+                while(num > subSet[j])
+                    j++;
+                
+                subSet[j] = num;
+            }
+        }
+        
+        return subSet.size() >= 3;
+    }
+};
