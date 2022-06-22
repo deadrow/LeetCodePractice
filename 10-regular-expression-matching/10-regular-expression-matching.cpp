9bbd626db5e9438bdc314ada1d9644bec369e598
deadrow <ankursingh.nit5@gmail.com>
2022-06-22 23:25:08 +0530

    Time: 35 ms (29.57%), Space: 22.7 MB (5.06%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> memo;
+    bool dp(string s, string p, int i, int j)
+    {
+        int sn = s.size();
+        int pn = p.size();
+        
+        if(j == pn)
+            return i == sn;
+        
+        if(memo[i][j] != -1)
+            return memo[i][j];
+        
+        bool firstMatch = (i < sn && (s[i] == p[j] || p[j] == '.'));
+        
+        if(j+1<pn && p[j+1] == '*')
+        {
+            // don't use * and just increment
+            if(dp(s, p, i, j+2))
+                return memo[i][j] = true;
+            
+            if(firstMatch && dp(s, p, i+1, j))
+                return memo[i][j] = true;
+        }
+        
+        if(firstMatch)
+            return memo[i][j] = dp(s, p, i+1, j+1);
+        
+        return memo[i][j] = false;
+    }
+    bool isMatch(string s, string p) {
+        memo.resize(s.size()+1, vector<int>(p.size()+1,-1));
+        return dp(s, p, 0, 0);
+    }
+};
