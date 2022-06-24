5c5c9ec12eed37153e4e0085cfb4f469b36c8b3f
deadrow <ankursingh.nit5@gmail.com>
2022-06-24 22:01:58 +0530

    Time: 233 ms (53.94%), Space: 9.8 MB (12.48%) - LeetHub

---
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -0,0 +1,23 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int celeb = 0;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(knows(celeb, i))
+                celeb = i;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(celeb != i && (knows(celeb, i) || !knows(i, celeb)))
+                return -1;
+        }
+        
+        return celeb;
+    }
+};
