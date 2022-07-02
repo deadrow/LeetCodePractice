65ca20549613c3cca47b9ef484e6888fb10f55c8
deadrow <ankursingh.nit5@gmail.com>
2022-07-02 11:24:48 +0530

    Time: 7 ms (44.64%), Space: 6.6 MB (62.34%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    int m, n;
-    int dp(string source, string target, int i, int j)
-    {
-        if(i == m || j == n)
-            return 0;
-        
-        if(source[i] == target[j])
-            return 1 + dp(source, target, i+1, j+1);
-        
-        return dp(source, target, i+1, j);
-    }
     
     int shortestWay(string source, string target) {
-        this->m = source.size();
-        this->n = target.size();
+        int m = source.size();
+        int n = target.size();
+        
+        unordered_map<char, bool> cache;
+        for(auto it : source)
+            cache[it] = true;
         
         int count = 0;
-        while (!target.empty()) {
-            int match = dp(source, target, 0, 0);
-            if(match == 0)
+        int i=0;
+        int j=0;
+        
+        int ans = 0;
+        while(j < n)
+        {
+            if(!cache[target[j]])
                 return -1;
             
-            count++;
-            target = target.substr(match);
+            while(i < m)
+            {
+                if(source[i] == target[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                    i++;
+            }
+            
+            ans += 1;
+            i= 0;
         }
         
-        return count;
+        return ans;
     }
 };
