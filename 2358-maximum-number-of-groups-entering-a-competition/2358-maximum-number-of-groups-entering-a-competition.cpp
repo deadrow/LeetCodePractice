49913cabd402a21eacadc4095ddd34500dd4689b
deadrow <ankursingh.nit5@gmail.com>
2022-07-31 22:57:57 +0530

    Time: 203 ms (100.00%), Space: 56.5 MB (100.00%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+        int k = 0;
+        int n = grades.size();
+        
+        int total = 0;
+        while(total + k + 1 <= n)
+        {
+            total += ++k;
+        }
+        
+        return k;
+    }
+};
