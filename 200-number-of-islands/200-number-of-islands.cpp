d6faa220d2ab6724f1665a6db426f4c22b740461
deadrow <ankursingh.nit5@gmail.com>
2022-08-07 22:39:24 +0530

    Time: 46 ms (71.09%), Space: 12.4 MB (55.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -12,25 +12,6 @@ public:
             if(nRow >=0 && nRow < m && nCol >= 0 && nCol <n && grid[nRow][nCol] == '1')
                 dfs(nRow, nCol, m, n, grid);
         }
-//         if(row < m-1 && grid[row+1][col] == '1')
-//         {
-//             dfs(row+1, col, m, n, grid);
-//         }
-        
-//         if(row > 0 && grid[row-1][col] == '1')
-//         {
-//             dfs(row-1, col, m, n, grid);
-//         }
-        
-//         if(col < n-1 && grid[row][col+1] == '1')
-//         {
-//             dfs(row, col+1, m, n, grid);
-//         }
-        
-//         if(col > 0 && grid[row][col-1] == '1')
-//         {
-//             dfs(row, col-1, m, n, grid);
-//         }
     }
     
     int numIslands(vector<vector<char>>& grid)
