9035f4499ff39ff6a88e74629619449f1881ec96
deadrow <ankursingh.nit5@gmail.com>
2022-08-08 22:22:44 +0530

    10 ms

--- solutions/300. Longest Increasing Subsequence.cpp
+++ solutions/300. Longest Increasing Subsequence.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    vector<int>memo;
+    
+    // Old DP solution
+    int dp(vector<int>& nums, int cur_i, int prev, int n)
+    {
+        if(cur_i >= n)
+            return 0;
+        
+        if(memo[prev+1] != -1)
+            return memo[prev+1];
+        
+        int take = 0;
+        if(prev == -1 || nums[cur_i] > nums[prev])
+            take = 1 + dp(nums, cur_i+1, cur_i, n);
+        
+        int leave = dp(nums, cur_i+1, prev, n);
+        
+        return memo[prev+1] = max(take, leave);
+    }
+    
+    int lengthOfLIS1(vector<int>& nums) {
+        int n = nums.size();
+        memo.resize(n+1,-1);
+        return dp(nums, 0, -1, n);
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
         
         vector<int>subSet;
