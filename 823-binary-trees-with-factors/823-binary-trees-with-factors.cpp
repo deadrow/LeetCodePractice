7fb3d7c73f2c03eaa4acbb20b95e21accea2e6f8
deadrow <ankursingh.nit5@gmail.com>
2022-08-10 22:41:30 +0530

    Time: 153 ms (31.08%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -13,11 +13,14 @@ public:
         int n = arr.size();
         
         long total = 0;
-        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<i;j++)
             {
+                // For big number we need to find all combination of
+                // its factors. For eg [2,3,6,18]
+                // for 6 count would be total ways to make 2 * total
+                // ways to make 3
                 int first = arr[i];
                 int second = arr[i]/arr[j];
                 if(arr[i]%arr[j] == 0 && dict.find(second) != dict.end())
@@ -28,12 +31,6 @@ public:
             
             total = (total + dict[arr[i]]) % MOD;
         }
-        
-//         long total = 0;
-//         for(auto it : arr)
-//         {
-//             total +=dict[it];
-//         }
         
         return total;
     }
