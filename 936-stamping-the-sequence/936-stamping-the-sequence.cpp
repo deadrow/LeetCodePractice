2633a241a98cacd6f5b031126be86ee535e60626
deadrow <ankursingh.nit5@gmail.com>
2022-08-21 12:28:57 +0530

    Time: 23 ms (59.02%), Space: 11.1 MB (42.62%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool isMatch(string& a, string& b)
+    {
+        if(a.size() != b.size())
+            return false;
+        
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i] != b[i] && a[i] != '?' && b[i] != '?')
+                return false;
+        }
+        
+        return true;
+    }
+    
+    bool allWildCards(string& a)
+    {
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i] != '?')
+                return false;
+        }
+        
+        return true;
+    }
+    
+    vector<int> movesToStamp(string stamp, string target) {
+        int m = stamp.size();
+        int n = target.size();
+        
+        vector<int> ret;
+        
+        while(true)
+        {
+            // Find atleaset 1 match
+            bool matchFound = false;
+            for(int i=0;i<n-m+1;i++)
+            {
+                string cur = target.substr(i, m);
+
+                if(allWildCards(cur))
+                    continue;
+                
+                if(isMatch(stamp, cur))
+                {
+                    matchFound = true;
+                    ret.push_back(i);
+
+                    for(int j=0;j<m;j++)
+                        target[i+j] = '?';
+                }
+            }
+            
+            if(!matchFound)
+                break;
+        }
+        
+        if(!allWildCards(target)) return vector<int>();
+        
+        reverse(ret.begin(), ret.end());
+        
+        return ret;
+
+    }
+};
