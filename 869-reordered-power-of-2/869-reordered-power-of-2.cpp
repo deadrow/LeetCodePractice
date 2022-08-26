9b88b3442d0d14a8fcc512273d95d8dc342bf035
deadrow <ankursingh.nit5@gmail.com>
2022-08-26 10:39:02 +0530

    Time: 4 ms (61.58%), Space: 6 MB (83.90%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -24,9 +24,40 @@ public:
             }
         }
     }
-    bool reorderedPowerOf2(int n) {
+    bool reorderedPowerOf21(int n) {
         string cur = to_string(n);
         permute(cur, 0);
         return powerOfTwo;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        string cur = to_string(n);
+        int digits[10] = {0};
+        
+        for(auto it : cur)
+            digits[it-'0']++;
+        
+        for(int i=0;i<31;i++)
+        {
+            string next = to_string(1<<i);
+            int curDigits[10] = {0};
+            for(auto it : next)
+                curDigits[it-'0']++;
+            
+            bool matched = true;
+            for(int i=0;i<10;i++)
+            {
+                if(curDigits[i] != digits[i])
+                {
+                    matched = false;
+                    break;
+                }
+            }
+            
+            if(matched)
+                return true;
+        }
+        
+        return false;
     }
 };
