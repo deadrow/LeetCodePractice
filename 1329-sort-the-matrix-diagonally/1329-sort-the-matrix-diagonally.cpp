bbb673678c81f1403efff339fcc112a7d736ee3b
deadrow <ankursingh.nit5@gmail.com>
2022-08-28 10:34:21 +0530

    Time: 34 ms (9.83%), Space: 11.2 MB (5.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -14,17 +14,15 @@ public:
             }
         }
         
-        vector<vector<int>> ret(m, vector<int>(n));
-        
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                ret[i][j] = *dict[i-j].begin();
+                mat[i][j] = *dict[i-j].begin();
                 dict[i-j].erase(dict[i-j].begin());
             }
         }
         
-        return ret;
+        return mat;
     }
 };
