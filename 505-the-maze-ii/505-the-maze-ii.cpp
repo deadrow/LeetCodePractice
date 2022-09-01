6fcaaeeb936fd6ad1011cdcef755be182f80afa4
deadrow <ankursingh.nit5@gmail.com>
2022-09-01 22:52:33 +0530

    Time: 209 ms (18.56%), Space: 21.5 MB (40.12%) - LeetHub

--- 505-the-maze-ii/505-the-maze-ii.cpp
+++ 505-the-maze-ii/505-the-maze-ii.cpp
@@ -7,48 +7,41 @@ public:
         
         vector<vector<bool>> visited(m, vector<bool>(n));
         
-        auto compare = [](const vector<int>& a, const vector<int>& b)
-        {
-            return a[0] > b[0];
-        };
-        
-        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> bfs(compare);
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> bfs;
         bfs.push({0,start[0],start[1]});
+        
+        while(!bfs.empty())
+        {
+            int curDistance = bfs.top()[0];
+            int row = bfs.top()[1];
+            int col = bfs.top()[2];
+            bfs.pop();
+            
+            if(visited[row][col])
+                continue;
+            
+            visited[row][col] = true;
 
-		while(!bfs.empty())
-		{
-			auto top = bfs.top();
-			bfs.pop();
-			
-			int curDistance = top[0];
-			int row = top[1];
-			int col = top[2];
-			
-			if(visited[row][col])
-				continue;
-			
-			visited[row][col] = true;
+            if(row == destination[0] && col == destination[1])
+                return curDistance;
 
-			if(row == destination[0] && col == destination[1])
-				return curDistance;
+            for(auto it : directions)
+            {
+                int nRow = row + it.first;
+                int nCol = col + it.second;
+                
+                int steps = 0;
+                while(nRow >= 0 && nRow < m && nCol >=0 && nCol < n && maze[nRow][nCol] == 0)
+                {
+                    nRow += it.first;
+                    nCol += it.second;
+                    steps++;
+                }
+                
 
-			for(auto it : directions)
-			{
-				int nRow = row;
-				int nCol = col;
-				
-				int steps = 0;
-				while(nRow+it.first >= 0 && nRow+it.first < m && nCol+it.second >=0 && nCol+it.second < n && maze[nRow+it.first][nCol+it.second] == 0)
-				{
-					steps++;
-					nRow += it.first;
-					nCol += it.second;
-				}
-				
-				// ball hit the wall.
-				bfs.push({curDistance+steps, nRow, nCol});
-			}
-		}
+                bfs.push({curDistance+steps, nRow-it.first, nCol-it.second});
+            }
+        }
         
         return -1;
     }
