6b24c9f8676d933351a3cf17a3eaf50a2edb2e4e
deadrow <ankursingh.nit5@gmail.com>
2022-09-05 21:32:04 +0530

    Time: 3 ms (44.46%), Space: 6.2 MB (49.54%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        if(password.empty() || password.size() < 8)
+            return false;
+        
+        bool oneUpper = false;
+        bool oneLower = false;
+        bool oneDigit = false;
+        bool oneSpecial = false;
+        
+        for(int i=0;i<password.size();i++)
+        {
+            if(i == 0 || password[i-1] != password[i])
+            {
+                if(isupper(password[i]))
+                    oneUpper = true;
+                else if(islower(password[i]))
+                    oneLower = true;
+                else if(isdigit(password[i]))
+                    oneDigit = true;
+                else
+                {
+                    string specials("!@#$%^&*()-+");
+                    if(specials.find(password[i]) != string::npos)
+                        oneSpecial = true;
+                }
+            }
+            else
+                return false;
+        }
+        
+        if(oneUpper && oneLower && oneDigit && oneSpecial)
+            return true;
+        
+        return false;
+    }
+};
