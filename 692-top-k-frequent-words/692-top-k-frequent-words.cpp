c4e3ce25a4fa235ba568622b750d6b25c61e408c
deadrow <ankursingh.nit5@gmail.com>
2022-09-05 22:01:42 +0530

    Time: 49 ms (5.23%), Space: 12.6 MB (74.29%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -9,22 +9,28 @@ public:
         auto compare = [](const pair<int, string>& a, const pair<int, string>& b)
         {
             if(a.first == b.first)
-                return a.second > b.second;
-            return a.first < b.first;
+                return a.second < b.second;
+            return a.first > b.first;
         };
         
         priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(compare)>pq(compare);
         
         for(auto it : dict)
+        {
             pq.push({it.second, it.first});
+            if(pq.size() > k)
+                pq.pop();
+        }
+            
         
         vector<string> ret;
-        while(!pq.empty() && k--)
+        while(!pq.empty())
         {
             ret.push_back(pq.top().second);
             pq.pop();
         }
-        
+
+        reverse(ret.begin(), ret.end());
         return ret;
     }
 };
