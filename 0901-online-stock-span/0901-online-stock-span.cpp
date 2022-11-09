33905695b8d0f51df16ede259b5124d8659a4d7d
deadrow <ankursingh.nit5@gmail.com>
2022-11-09 10:50:03 +0530

    Time: 226 ms (96.03%), Space: 84.3 MB (22.76%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,21 +1,25 @@
 class StockSpanner {
 public:
-    vector<int>data;
+    stack<pair<int,int>>data;
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int days = 0;
-        data.push_back(price);
-        
-        for(int i=data.size()-1;i>=0;i--)
+        if(data.empty() || data.top().first > price)
         {
-            if(price >= data[i])
-                days++;
-            else
-                break;
+            data.push({price, 1});
+            return 1;
         }
+            
+        int days = 1;
+        while(!data.empty() && data.top().first <= price)
+        {
+            days += data.top().second;
+            data.pop();
+        }
+        
+        data.push({price, days});
         
         return days;
     }
