63d87b3a9c725f7e0ab94123f5808c214440f298
deadrow <ankursingh.nit5@gmail.com>
2022-11-11 22:49:01 +0530

    Time: 792 ms (50.64%), Space: 168 MB (27.44%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        map<string,int>dict;
+        
+        for(auto it : words)
+            dict[it]++;
+        
+        int count = 0;
+        bool used = false;
+        for(auto it : dict)
+        {
+            string cur = it.first;
+            string rev(cur.rbegin(), cur.rend());
+            if(cur == rev)
+            {
+                int freq = dict[cur];
+                if(!used && freq & 1)
+                {
+                    used = true;
+                    count += (freq/2)*4 + 2;
+                }
+                else
+                    count += (freq/2)*4;
+                    
+            }
+            else if(dict.find(rev) != dict.end())
+                count += min(dict[cur], dict[rev])*2;            
+        }
+        
+        return count;
+    }
+};
