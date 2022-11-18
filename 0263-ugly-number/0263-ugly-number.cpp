1bfb79736254d9aa4c87a27f1633206854e8bb03
deadrow <ankursingh.nit5@gmail.com>
2022-11-18 10:06:20 +0530

    Time: 7 ms (20.99%), Space: 6.1 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0)
+            return false;
+        
+        vector<int> divisors{2,3,5};
+        for(auto it : divisors)
+        {
+            while(n % it == 0)
+                n /= it;
+        }
+
+        return n == 1;
+    }
+};
