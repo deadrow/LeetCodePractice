39d0ee80f78634782d77206e4592743ffcd0cbe2
deadrow <ankursingh.nit5@gmail.com>
2022-11-20 21:43:43 +0530

    Time: 18 ms (66.14%), Space: 8.1 MB (72.15%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int>st;
+        
+        int sign = 1;
+        long curNum = 0;
+        int res = 0;
+        
+        for(auto i : s)
+        {
+            if(isdigit(i))
+            {
+                curNum = curNum*10 + i-'0';
+            }
+            else 
+            {
+                if(i == '+')
+                {
+                    res += sign*curNum;
+                    sign = 1;
+                    curNum = 0;
+                }
+                else if(i == '-')
+                {
+                    res += sign*curNum;
+                    sign = -1;
+                    curNum = 0;
+                }
+                else if(i == '(')
+                {
+                    st.push(res);
+                    st.push(sign);
+                    sign = 1;
+                    res = 0;
+                }
+                else if(i == ')')
+                {
+                    res += sign*curNum;
+                    res *= st.top();
+                    st.pop();
+                    res += st.top();
+                    st.pop();
+                    curNum = 0;
+                }
+            }
+        }
+        
+        if(curNum != 0)
+            res += sign*curNum;
+        return res;
+    }
+};
