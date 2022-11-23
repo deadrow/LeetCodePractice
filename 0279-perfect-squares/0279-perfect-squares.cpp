28622840c8b022ddd925417dc48ce13b596f9e9a
deadrow <ankursingh.nit5@gmail.com>
2022-11-23 17:40:45 +0530

    Time: 26 ms (94.78%), Space: 7 MB (85.74%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    vector<int>squares;
-    vector<int>memo;
-    int dp(int target)
-    {
-        if(target == 0)
-            return 0;
-        
-        if(memo[target] != -1)
-            return memo[target];
-        
-        int ret = INT_MAX;
-        for(auto it : squares)
-        {
-            if(target >= it)
-            {
-                ret = min(ret, dp(target-it)+1);
-            }
+    set<int> square_nums;
+public:
+    bool is_divided_by(int n, int count) {
+      if (count == 1) {
+        return square_nums.count(n);
+      }
+
+      for (auto square : square_nums) {
+        if (is_divided_by(n - square, count - 1)) {
+          return true;
         }
-        
-        return memo[target] = ret;
+      }
+      return false;
     }
-    
+
     int numSquares(int n) {
-        
-        int i = 1;
-        while(i*i <= n)
-        {
-            squares.push_back(i*i);
-            i++;
-        }
-        
-        memo.resize(n+1, -1);
-        return dp(n);
+      square_nums.clear();
+
+      for (int i = 1; i * i <= n; ++i) {
+        square_nums.insert(i * i);
+      }
+
+      int count = 1;
+      for (; count <= n; ++count) {
+        if (is_divided_by(n, count))
+          return count;
+      }
+      return count;
     }
 };
