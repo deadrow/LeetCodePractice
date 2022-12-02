e8150186997b0ced8c283965e36c7179fec4bb16
deadrow <ankursingh.nit5@gmail.com>
2022-12-02 21:40:39 +0530

    Time: 193 ms (63.47%), Space: 20.8 MB (16.77%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        map<char, int> first;
+        map<char, int> second;
+        
+        for(auto it : word1)
+            first[it]++;
+        
+        for(auto it : word2)
+            second[it]++;
+        
+        if(first.size() != second.size())
+            return false;
+        
+        // check if chars are same on not
+        auto it1 = first.begin();
+        auto it2 = second.begin();
+        while(it1 != first.end())
+        {
+            if(it1->first != it2->first)
+                return false;
+            it1++;
+            it2++;
+        }
+        
+        vector<int>countFirst;
+        for(auto it : first)
+            countFirst.push_back(it.second);
+        
+        vector<int>countSecond;
+        for(auto it : second)
+            countSecond.push_back(it.second);
+        
+        sort(countFirst.begin(), countFirst.end());
+        sort(countSecond.begin(), countSecond.end());
+        
+        for(int i=0;i<countFirst.size();i++)
+        {
+            if(countFirst[i] != countSecond[i])
+                return false;
+        }
+        
+        return true;
+    }
+};
