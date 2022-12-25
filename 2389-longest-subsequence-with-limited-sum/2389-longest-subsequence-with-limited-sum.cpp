6e2161a953af51ad08ce13e7a081ca3b1c7f2e03
deadrow <ankursingh.nit5@gmail.com>
2022-12-25 18:54:54 +0530

    Time: 17 ms (94.40%), Space: 14 MB (21.90%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -3,6 +3,23 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         
+        vector<int> prefix(nums);
+        for(int i=1;i<nums.size();i++)
+            prefix[i] = nums[i] + prefix[i-1];
+        
+        vector<int> result;
+        for(auto it : queries)
+        {
+            auto it1 = upper_bound(prefix.begin(), prefix.end(), it);
+            result.push_back(it1-prefix.begin());
+        }
+        
+        return result;
+    }
+    
+    vector<int> answerQueries1(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        
         vector<int> result;
         for(auto it : queries)
         {
