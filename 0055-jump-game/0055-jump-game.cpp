c28f64bd8dd21003a2c6f85a5445a3609bf9f34e
deadrow <ankursingh.nit5@gmail.com>
2022-12-26 18:22:32 +0530

    Time: 1170 ms (15.25%), Space: 51.4 MB (6.72%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int>memo;
+    bool dp(vector<int>& nums, int i)
+    {
+        if(i >= nums.size()-1)
+            return true;
+        
+        if(memo[i] != -1)
+            return memo[i];
+        
+        int maxJump = min((int)nums.size()-1, nums[i]);
+        for(int j=i+1;j<=i+maxJump;j++)
+        {
+            if(dp(nums, j))
+                return memo[i] = true;
+        }
+        
+        return memo[i] = false;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        if(nums.size() == 1)
+            return true;
+        
+        memo.resize(nums.size(), -1);
+        return dp(nums, 0);
+    }
+};
