bc3a9ba0468e9de6f49a3cd8735837bdf30d5c96
deadrow <ankursingh.nit5@gmail.com>
2022-12-30 15:24:05 +0530

    Time: 39 ms (30.32%), Space: 19.4 MB (12.18%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> result;
+        
+        queue<vector<int>> bfs;
+        bfs.push({0});
+        
+        while(!bfs.empty())
+        {
+            int size = bfs.size();
+            for(int i=0;i<size;i++)
+            {
+                auto top = bfs.front();
+                bfs.pop();
+                
+                if(top.back() == n-1)
+                {
+                    result.push_back(top);
+                    continue;
+                }
+                
+                for(auto it : graph[top.back()])
+                {
+                    vector cur(top);
+                    cur.push_back(it);
+                    bfs.push(cur);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
