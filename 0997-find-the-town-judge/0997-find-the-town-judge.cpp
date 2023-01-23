468f54580156e8098aff4d086d34ec9378972ec6
deadrow <ankursingh.nit5@gmail.com>
2023-01-23 14:57:01 +0530

    Time: 195 ms (74.82%), Space: 69 MB (29.65%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -7,12 +7,12 @@ public:
         for(auto it : trust)
         {
             indegree[it[1]]++;
-            outdegree[it[0]]++;
+            indegree[it[0]]--;
         }
         
         for(int i=1;i<=n;i++)
         {
-            if(indegree[i] == n-1 && outdegree[i] == 0)
+            if(indegree[i] == n-1)
                 return i;
         }
         
