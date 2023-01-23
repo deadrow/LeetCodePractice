37a0a8ea777575844e2c7c9231167b2f65504b0b
deadrow <ankursingh.nit5@gmail.com>
2023-01-23 13:35:17 +0530

    Time: 812 ms (5.01%), Space: 99.5 MB (5.05%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n == 1)
+            return 1;
+        
+        map<int, set<int>>dict;
+        
+        for(auto it : trust)
+            dict[it[1]].insert(it[0]);
+        
+        int counts = 0;
+        
+        int ret = -1;
+        for(auto it : dict)
+        {
+            // check if any dict has n-1 size
+            // it should be only one dict
+            if(it.second.size() == n-1)
+            {
+                for(auto it1 : it.second)
+                {
+                    if(dict.find(it1) != dict.end())
+                    {
+                        if(dict[it1].find(it.first) != dict[it1].end())
+                            return -1;
+                    }  
+                }
+                
+                ret = it.first;
+            }
+        }
+        
+        return ret;
+    }
+};
