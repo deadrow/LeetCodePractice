90022c5e117998fade9a0a7e517659fbb0985cd9
deadrow <ankursingh.nit5@gmail.com>
2023-01-26 19:13:42 +0530

    Time: 43 ms (43.88%), Space: 12.6 MB (95.82%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice1(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adjList(n);
         
         for(auto it : flights)
@@ -41,5 +41,27 @@ public:
         
         
         return dist[dst] != INT_MAX ? dist[dst] : -1;
+    }
+    
+    // Bellman ford
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // Distance from source to all other nodes.
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        // Run only K+1 times since we want shortest distance in K hops.
+        for (int i = 0; i <= k; i++) 
+        {
+            vector<int>temp(dist);
+            for (auto& flight : flights) 
+            {
+                if(dist[flight[0]] != INT_MAX)
+                    temp[flight[1]] = min(temp[flight[1]], dist[flight[0]] + flight[2]);
+            }
+            
+            dist = temp;
+        }
+        
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
