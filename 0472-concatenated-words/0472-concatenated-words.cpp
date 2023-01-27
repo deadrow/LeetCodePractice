3f193b1c775cad5d95af911daf336ea004dbce37
deadrow <ankursingh.nit5@gmail.com>
2023-01-27 11:24:31 +0530

    Time: 310 ms (57.17%), Space: 47.6 MB (62.94%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     unordered_set<string>wordMap;
-    
+    vector<int>memo;
     bool dfs(string word, int start, vector<string>& splits)
     {
         if(start >= word.size())
             return true;
+        
+        if(memo[start] != -1)
+            return memo[start];
         
         for(int i=start+1;i<=word.size();i++)
         {
@@ -13,25 +16,29 @@ public:
             if(wordMap.find(cur) != wordMap.end() && dfs(word, i, splits))
             {
                 splits.push_back(cur);
-                return true;
+                return memo[start] = true;
             }
         }
         
-        return false;
+        return memo[start] = false;
     }
     
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         for(auto it : words)
             wordMap.insert(it);
         
+        
         vector<string> res;
         
         for(auto it : words)
         {
+            memo.resize(it.size()+1, -1);
             vector<string>splits;
             bool ret = dfs(it, 0, splits);
             if(ret && splits.size() > 1)
                 res.push_back(it);
+            
+            memo.clear();
         }
         
         return res;
