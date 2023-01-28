2bd7e8911ef1ee28384f38c0bb99681e72378724
deadrow <ankursingh.nit5@gmail.com>
2023-01-28 19:50:17 +0530

    Time: 94 ms (41.57%), Space: 33.5 MB (54.90%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -31,17 +31,6 @@ public:
             prev = it;
         }
         
-//         res.push_back(intervals[0]);
-//         for(int j=1;j<intervals.size();j++)
-//         {
-//             if(intervals[j][0]-1 == res.back()[1])
-//             {
-//                 res.back()[1] = max(intervals[j][1],res.back()[1]);
-//             }
-//             else
-//                 res.push_back(intervals[j]);
-//         }
-        
         return res;
     }
 };
