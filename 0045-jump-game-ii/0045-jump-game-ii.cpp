b469f7ea4b529698d74cc02e75cd8022288eab60
deadrow <ankursingh.nit5@gmail.com>
2023-02-08 11:20:36 +0530

    Time: 439 ms (28.21%), Space: 17.2 MB (29.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int>memo;
+    
+    // TOP down
     int dp(vector<int>& nums, int cur_i, int n)
     {
         if(cur_i >= n)
@@ -20,8 +22,24 @@ public:
         
         return memo[cur_i] = cur;
     }
-    int jump(vector<int>& nums) {
+    int jump1(vector<int>& nums) {
         memo.resize(nums.size(), -1);
         return dp(nums,0, nums.size()-1);
+    }
+    
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+		vector<int>dp(n, INT_MAX);
+		
+		dp[0] = 0; // because we need 0 steps to reach 0th pos
+		
+		for(int i=1;i<n;i++)
+		{
+			int steps = nums[i-1];
+			for(int j=i;j<min(n, i+steps);j++)
+				dp[j] = min(dp[j], dp[i-1]+1);
+		}
+		
+		return dp[n-1];
     }
 };
