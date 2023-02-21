8a4bf45b9e307da059ae4edccc135d1162aebd0a
deadrow <ankursingh.nit5@gmail.com>
2023-02-21 21:35:20 +0530

    Time: 28 ms (67.14%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int ans = 0;
-        for(auto it : nums)
+        for(auto& it : nums)
             ans ^= it;
         
         return ans;
