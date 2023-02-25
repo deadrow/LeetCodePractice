67553012cdbbc23fd49a6c019e7ac7a7cb4f5673
deadrow <ankursingh.nit5@gmail.com>
2023-02-25 16:28:39 +0530

    Time: 52 ms (60.71%), Space: 19.8 MB (8.31%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // binary search template from 
+        // https://leetcode.com/discuss/general-discussion/786126/Python-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems
+        
+		auto feasible = [=](int capacity){
+			int days = 0;
+			for(auto it : piles)
+			{
+				if(it <= capacity)
+					days += 1;
+				else
+					days += ceil((double)it/capacity);
+			}
+			
+			return days <= h;
+		};
+        
+		int left = 1;
+		int right = *max_element(piles.begin(), piles.end());
+        
+        while(left < right)
+        {
+            int mid = left + (right-left)/2;
+            if(feasible(mid))
+                right = mid;
+            else
+                left = mid+1;
+        }
+        
+        return left;
+    }
+};
