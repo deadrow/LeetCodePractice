4e9a85325e0908e7feb573a61ec65ca74c3cdd2f
deadrow <ankursingh.nit5@gmail.com>
2023-03-01 22:00:52 +0530

    Time: 213 ms (54.28%), Space: 66 MB (38.21%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<>>pq;
+        for(auto it : nums)
+            pq.push(it);
+        
+        nums.clear();
+        
+        while(!pq.empty())
+        {
+            nums.push_back(pq.top());
+            pq.pop();
+        }
+        
+        return nums;
+    }
+};
