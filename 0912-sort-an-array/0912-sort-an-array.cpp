10b110ff511b2b2caa01d685e24888d8ba213074
deadrow <ankursingh.nit5@gmail.com>
2023-03-01 22:11:25 +0530

    Time: 375 ms (34.09%), Space: 120.6 MB (28.85%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<>>pq;
-        for(auto it : nums)
-            pq.push(it);
+        auto heapSort = [&](){
+            priority_queue<int, vector<int>, greater<>>pq;
+            for(auto it : nums)
+                pq.push(it);
+
+            nums.clear();
+
+            while(!pq.empty())
+            {
+                nums.push_back(pq.top());
+                pq.pop();
+            }
+        };
         
-        nums.clear();
+        // heapSort();
         
-        while(!pq.empty())
-        {
-            nums.push_back(pq.top());
-            pq.pop();
-        }
+        std::function<void(vector<int>::iterator,vector<int>::iterator)> mergeSort = [&](auto low, auto high){
+            if(high-low > 1)
+            {
+                auto mid = low + (high-low)/2;
+                mergeSort(low, mid);
+                mergeSort(mid, high);
+                inplace_merge(low, mid, high);
+            }
+        };
         
+        mergeSort(nums.begin(), nums.end());
         return nums;
     }
 };
