fa4a1d85ba202ed9a090e821a67bb4aa8dcbd56e
deadrow <ankursingh.nit5@gmail.com>
2023-03-20 21:08:45 +0530

    Time: 41 ms (20.18%), Space: 63.3 MB (6.86%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -11,8 +11,8 @@
  */
 class Solution {
 public:
-    unordered_map<string, pair<TreeNode*, int>>dict;
-    
+    unordered_map<string, int>dict;
+    vector<TreeNode*> ret;
     string dfs(TreeNode* root)
     {
         if(!root)
@@ -22,26 +22,16 @@ public:
         string right = dfs(root->right);
         
         string key = "(" + left + ")" + to_string(root->val) + "(" + right + ")";
-        if(dict.find(key) == dict.end())
-            dict[key] = {root, 1};
-        else
-        {
-            dict[key] = {root, dict[key].second+1};
-        }
-        
+        dict[key]++;
+        if(dict[key] == 2)
+            ret.push_back(root);
+
         return key;
     }
     
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
         dfs(root);
-        
-        vector<TreeNode*> ret;
-        
-        for(auto it : dict)
-        {
-            if(it.second.second > 1)
-                ret.push_back(it.second.first);
-        }
+
         return ret;
     }
 };
