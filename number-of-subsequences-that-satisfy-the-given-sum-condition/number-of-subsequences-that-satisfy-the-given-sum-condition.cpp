f6cdf400c39da93374a3962bb55ac2c2ed826b20
deadrow <ankursingh.nit5@gmail.com>
2023-05-06 22:31:56 +0530

    Time: 135 ms (73.07%) | Memory: 49.8 MB (68.93%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -5,6 +5,14 @@ public:
 
         int MOD = 1000000007;
         int n = nums.size();
+
+        // Two pointer approach
+        // Find i and j such that nums[i]+nums[j] <= target
+        // Then the subsequences would be 2pow(j-i) because
+        // we are only taking min and max. In between all the
+        // numbers would be part of that powerset
+        
+        // precalculate otherwise it is giving TLE
         vector<int>pow(n);
         pow[0] = 1;
         for(int i=1;i<n;i++)
