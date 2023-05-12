fa47c939c74b8a808560295ed540f9bfe6d3057c
deadrow <ankursingh.nit5@gmail.com>
2023-05-12 17:11:11 +0530

    Time: 5 ms (46.74%) | Memory: 8.3 MB (35.01%) - LeetSync

---
+++ maximum-population-year/maximum-population-year.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        map<int,int>dict;
+        for(auto it: logs)
+        {
+            dict[it[0]] += 1;
+            dict[it[1]] += -1;
+        }
+
+        int maxOverLap = 0;
+        int curOverLap = 0;
+        int start = 0;
+        for(auto it : dict)
+        {
+            curOverLap += it.second;
+            if(curOverLap > maxOverLap)
+            {
+                start = it.first;
+                maxOverLap = curOverLap;
+            }
+        }
+
+        return start;
+    }
+};
