17f820f429b9c19f719003085cb61b23641dd0be
deadrow <ankursingh.nit5@gmail.com>
2023-05-15 10:19:16 +0530

    Time: 46 ms (82.28%) | Memory: 20.3 MB (10.61%) - LeetSync

---
+++ matchsticks-to-square/matchsticks-to-square.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int>memo;
+    int targetSum = 0;
+    vector<int>sides;
+
+    bool dfs(vector<int>& matchsticks, int mask, int target, int k)
+    {
+        if(k == 1)
+            return true;
+        
+        if(memo[mask] != -1)
+            return memo[mask];
+
+        if(target == 0)
+            return dfs(matchsticks, mask, targetSum, k-1);
+
+        for(int i=0;i<matchsticks.size();i++)
+        {
+            if((mask >> i) & 1)
+                continue;
+            
+            if(target - matchsticks[i] < 0)
+                continue;
+
+            mask = mask | (1 << i);
+            if(dfs(matchsticks, mask, target - matchsticks[i], k))
+                return memo[mask] = true;
+             mask = mask ^ (1 << i);
+        }
+
+        return memo[mask] = false;
+    }
+
+    bool makesquare(vector<int>& matchsticks) {
+        int totalSum = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+
+        if(totalSum % 4 != 0)
+            return false;
+
+        targetSum = totalSum/4;
+
+        int mask = 0;
+        sides.resize(4, 0);
+        memo.resize(1 << matchsticks.size(), -1);
+        return dfs(matchsticks, mask, targetSum, 4);
+    }
+};
