edc12d4d1be427a0647448b1147be01ccf676109
deadrow <ankursingh.nit5@gmail.com>
2023-05-20 11:46:07 +0530

    Time: 10 ms (5.49%) | Memory: 8.7 MB (10.92%) - LeetSync

--- evaluate-division/evaluate-division.cpp
+++ evaluate-division/evaluate-division.cpp
@@ -25,7 +25,7 @@ public:
         return false;
     }
 
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+    vector<double> calcEquation1(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
 
         map<string, vector<pair<string, double>>>adjMap;
 
@@ -42,6 +42,47 @@ public:
             unordered_set<string>visited;
             bool notPresent = (adjMap.count(queries[i][0]) == 0 || adjMap.count(queries[i][1]) == 0);
             if(notPresent || !dfs(adjMap, queries[i][0], queries[i][1], visited, 1.0, i))
+                result[i] = -1.0;
+        }
+
+        return result;
+    }
+
+    // Floyd warshall approach
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+
+        map<string, map<string, double>>adjMap;
+
+        for(int i=0;i<equations.size();i++)
+        {
+            string from = equations[i][0];
+            string to = equations[i][1];
+            adjMap[from][to] = values[i];
+            adjMap[to][from] = 1.0/values[i];
+            adjMap[to][to] = adjMap[from][from]= 1.0;
+        }
+
+        result.resize(queries.size());
+
+        // floyed warshall
+        for(auto k : adjMap)
+        {
+            for(auto i : adjMap[k.first])
+            {
+                for(auto j : adjMap[k.first])
+                {
+                    adjMap[i.first][j.first] = adjMap[i.first][k.first]* adjMap[k.first][j.first];
+                }
+            }
+        }
+
+        for(int i=0;i<queries.size();i++)
+        {
+            string from = queries[i][0];
+            string to = queries[i][1];
+            if(adjMap.find(from) != adjMap.end() && adjMap[from].find(to) != adjMap[from].end())
+                result[i] = adjMap[from][to];
+            else
                 result[i] = -1.0;
         }
 
