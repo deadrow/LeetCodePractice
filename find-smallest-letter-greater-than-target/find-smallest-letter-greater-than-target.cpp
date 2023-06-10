93713d12bfaeb6a4aec25084f83f274c8f905ea5
deadrow <ankursingh.nit5@gmail.com>
2023-06-10 22:47:47 +0530

    Time: 14 ms (79.08%) | Memory: 16 MB (31.84%) - LeetSync

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         auto it = upper_bound(letters.begin(), letters.end(), target);
-        if(it == letters.end())
-            return letters[0];
-        return letters[it-letters.begin()];
+        return it == letters.end() ? letters[0] : letters[it-letters.begin()];
     }
 };
