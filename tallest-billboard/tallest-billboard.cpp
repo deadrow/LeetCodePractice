6d160a3a8ec020886e20014b7322612cb96dee5f
deadrow <ankursingh.nit5@gmail.com>
2023-06-24 12:44:21 +0530

    Time: 153 ms (38.33%) | Memory: 72.9 MB (29.52%) - LeetSync

--- tallest-billboard/tallest-billboard.cpp
+++ tallest-billboard/tallest-billboard.cpp
@@ -17,14 +17,14 @@ public:
         // leave
         int leave = solve(rods, i+1, leftSum, rightSum);
         // add to left
-        int toLeft = solve(rods, i+1, leftSum + rods[i], rightSum) + rods[i]; // count the result only 1 time
+        int toLeft = solve(rods, i+1, leftSum + rods[i], rightSum);
         // add to right
         int toRight = solve(rods, i+1, leftSum , rightSum + rods[i]);
-        return memo[i][leftSum-rightSum + MaxSum] = max({leave, toLeft, toRight});
+        return memo[i][leftSum-rightSum + MaxSum] = max({leave, toLeft+rods[i], toRight+rods[i]});
     }
 
     int tallestBillboard(vector<int>& rods) {
         memo.resize(rods.size(), vector<int>(MaxSum*2, -1));
-        return solve(rods, 0, 0, 0);
+        return solve(rods, 0, 0, 0)/2;
     }
 };
