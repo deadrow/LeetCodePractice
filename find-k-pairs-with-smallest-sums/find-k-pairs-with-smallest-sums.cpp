8c21d6f3887f1e7d5fb3a3b50c1a4b53f5afb25c
deadrow <ankursingh.nit5@gmail.com>
2023-07-01 17:41:48 +0530

    Time: 386 ms (46.44%) | Memory: 114.5 MB (32.72%) - LeetSync

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-    struct Node
-    {
-        int i{0};
-        int j{0};
-    };
-    
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        
-        auto compare = [&](const Node& a, const Node& b)
-        {
-            return (nums1[a.i] + nums2[a.j]) > (nums1[b.i] + nums2[b.j]);
-        };
-        
-        priority_queue<Node, vector<Node>, decltype(compare)>pq(compare);
-
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        pq.push({0, 0});
-        
-        set<pair<int,int>>visited;
         vector<vector<int>> ret;
-        while(!pq.empty() && ret.size() < k)
+
+        auto cmp = [&](const pair<int,int>& a, const pair<int,int>& b)
         {
-            int i = pq.top().i;
-            int j = pq.top().j;
+            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
+        };
+
+        set<pair<int,int>> visited;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)>pq(cmp);
+        pq.push({0,0});
+
+        while(ret.size() < k && !pq.empty())
+        {
+            int i = pq.top().first;
+            int j = pq.top().second;
             pq.pop();
-            
-            if(visited.find({i,j}) != visited.end())
+
+            if(visited.count({i, j}))
                 continue;
-            
-            visited.insert({i,j});
+
             ret.push_back({nums1[i], nums2[j]});
+            visited.insert({i, j});
             
-            if(i+1 < m)
-                pq.push({i+1,j});
-            
-            if(j+1 < n)
+            if(i+1 < nums1.size())
+                pq.push({i+1, j});
+
+            if(j+1 < nums2.size())
                 pq.push({i, j+1});
         }
-        
+
         return ret;
     }
 };
