ba5ce625ef3eb89fd48461ac1b7ab82eec6f9190
deadrow <ankursingh.nit5@gmail.com>
2023-07-16 12:05:39 +0530

    Time: 300 ms (90.68%) | Memory: 75.6 MB (64.14%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    unordered_map<string, int>memo;
 
+#if 0 // self
+    unordered_map<string, int>memo;
     int dp(vector<vector<int>>& events, int k, int cur_i, int prev_i)
     {
         string hash = to_string(cur_i) + "#" + to_string(prev_i) + "# " + to_string(k);
@@ -22,4 +23,32 @@ public:
         sort(events.begin(), events.end());
         return dp(events, k, 0, -1);
     }
+
+#else
+    vector<vector<int>>memo;
+    int dp(vector<vector<int>>& events, int k, int cur_i)
+    {
+        if(cur_i >= events.size() || k == 0)
+            return 0;
+        
+        if(memo[cur_i][k] != -1)
+            return memo[cur_i][k];
+
+        int leave = dp(events, k, cur_i+1);
+        
+        int nextIdx = upper_bound(events.begin(), events.end(), events[cur_i][1], 
+            [=](int t, const auto& a) {
+                return a[0] > t;
+            }) - events.begin();
+
+        int take = events[cur_i][2] + dp(events, k-1, nextIdx);
+
+        return memo[cur_i][k] = max(take, leave);
+    }
+    int maxValue(vector<vector<int>>& events, int k) {
+        memo.resize(events.size(), vector<int>(k+1, -1));
+        sort(events.begin(), events.end());
+        return dp(events, k, 0);
+    }
+#endif
 };
