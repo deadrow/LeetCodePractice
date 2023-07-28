74fd70a315226e4e3e7f7edc2cb60bca82c7ff62
deadrow <ankursingh.nit5@gmail.com>
2023-07-28 22:32:01 +0530

    Time: 4 ms (63.90%) | Memory: 7.6 MB (29.30%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    vector<vector<vector<int>>>memo;
-    int dp(vector<int>& nums, int left, int right, bool player1)
+    vector<vector<int>> memo;
+    int dp(vector<int>& nums, int left, int right)
     {
         if(left > right)
             return 0;
-
+            
         if(left == right)
-        {
-            return player1 ? nums[left] : -nums[left];
-        }
+            nums[left];
         
-        if(memo[left][right][player1] != -1)
-            return memo[left][right][player1];
+        if(memo[left][right] != -1)
+            return memo[left][right];
 
-        if(player1)
-        {
-            int takeFromLeft = nums[left] + dp(nums, left+1, right, false);
-            int takeFromRight = nums[right] + dp(nums, left, right-1, false);
-            return memo[left][right][player1] = max(takeFromLeft, takeFromRight);
-        }
-
-        int takeFromLeft = dp(nums, left+1, right, true) - nums[left];
-        int takeFromRight = dp(nums, left, right-1, true) - nums[right];
-        return memo[left][right][player1] = min(takeFromLeft, takeFromRight);
+        int takeFromLeft = nums[left] - dp(nums, left+1, right);
+        int takeFromRight = nums[right] - dp(nums, left, right-1);
+        return memo[left][right] = max(takeFromLeft, takeFromRight);
     }
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        memo.resize(n, vector<vector<int>>(n, vector<int>(2,-1)));
-        int res = dp(nums, 0, nums.size()-1, true);
+        memo.resize(n+1, vector<int>(n+1,-1));
+        int res = dp(nums, 0, n-1);
         return res >= 0;
     }
 };
