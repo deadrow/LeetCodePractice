9e2c3111758f72972d62a2318f3d4ca82805dbc7
deadrow <ankursingh.nit5@gmail.com>
2023-07-28 22:19:27 +0530

    Time: 7 ms (43.71%) | Memory: 8.6 MB (8.42%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<vector<int>>>memo;
     int dp(vector<int>& nums, int left, int right, bool player1)
     {
         if(left > right)
@@ -10,18 +11,23 @@ public:
             return player1 ? nums[left] : -nums[left];
         }
         
+        if(memo[left][right][player1] != -1)
+            return memo[left][right][player1];
+
         if(player1)
         {
             int takeFromLeft = nums[left] + dp(nums, left+1, right, false);
             int takeFromRight = nums[right] + dp(nums, left, right-1, false);
-            return max(takeFromLeft, takeFromRight);
+            return memo[left][right][player1] = max(takeFromLeft, takeFromRight);
         }
 
         int takeFromLeft = dp(nums, left+1, right, true) - nums[left];
         int takeFromRight = dp(nums, left, right-1, true) - nums[right];
-        return min(takeFromLeft, takeFromRight);
+        return memo[left][right][player1] = min(takeFromLeft, takeFromRight);
     }
     bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        memo.resize(n, vector<vector<int>>(n, vector<int>(2,-1)));
         int res = dp(nums, 0, nums.size()-1, true);
         return res >= 0;
     }
