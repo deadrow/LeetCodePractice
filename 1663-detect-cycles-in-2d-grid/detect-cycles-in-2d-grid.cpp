67133230ce2c7596a01e5ed23a8c0600e2c22fa9
deadrow <ankursingh.nit5@gmail.com>
2023-11-25 11:55:38 +0530

    Time: 481 ms (7.79%) | Memory: 245.7 MB (5.01%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<pair<int,int>>directions{{0,1},{0,-1},{1,0},{-1,0}};
+
+    bool containsCycle(vector<vector<char>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j] < 'a')
+                    continue;
+                queue<vector<int>>bfs;
+                char ch = grid[i][j];
+                bfs.push({i,j});
+
+                while(!bfs.empty())
+                {
+                    auto v = bfs.front();
+                    int r = v[0];
+                    int c = v[1];
+                    bfs.pop();
+
+                    if(grid[r][c] < 'a') return true;
+
+                    grid[r][c] -= 26;
+
+                    for(auto it : directions)
+                    {
+                        int nr = r + it.first;
+                        int nc = c + it.second;
+                        if(nr >= 0 and nr < m and nc >=0 and nc < n and grid[nr][nc] == ch)
+                        {
+                            bfs.push({nr,nc});
+                        } 
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
