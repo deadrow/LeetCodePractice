d5e0c614cdf49fc439b98d5f9fe91391d442444e
deadrow <ankursingh.nit5@gmail.com>
2023-12-15 15:28:44 +0530

    Time: 5 ms (34.87%) | Memory: 6.4 MB (59.29%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        set<int>factors;
+        for(int i=1;i<=ceil(sqrt(n));i++)
+        {
+            if(n%i == 0)
+            {
+                factors.insert(i);
+                factors.insert(n/i);
+            }
+        }
+
+        for(auto it : factors)
+            cout << it << " ";
+
+        if(k > factors.size())
+            return -1;
+
+        auto it = factors.begin();
+        advance(it, k-1);
+        return *it;
+    }
+};
