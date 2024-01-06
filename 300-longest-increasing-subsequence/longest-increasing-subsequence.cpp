315b31b8f7b46a2aa6b75302edba8951fce46c18
deadrow <ankursingh.nit5@gmail.com>
2024-01-06 21:25:33 +0530

    Time: 243 ms (52.49%) | Memory: 10.8 MB (80.88%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Top to bottom
     vector<int>memo;
     int dp(vector<int>& nums, int i, int prev)
     {
@@ -16,8 +17,25 @@ public:
         int leave = dp(nums, i+1, prev);
         return memo[prev+1] = max(take, leave);
     }
-    int lengthOfLIS(vector<int>& nums) {
+
+    int lengthOfLIS1(vector<int>& nums) {
         memo.resize(nums.size()+1, -1);
         return dp(nums, 0, -1);
+    }
+
+    // bottom up
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j] < nums[i])
+                    dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        } 
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
