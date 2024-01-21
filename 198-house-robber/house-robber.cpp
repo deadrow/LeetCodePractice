663a5cbde0f719cc6c47384bd511fc182d78d04a
deadrow <ankursingh.nit5@gmail.com>
2024-01-21 15:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // top down solution
     vector<int>memo;
     int dp(vector<int>& nums, int i)
     {
@@ -12,8 +13,23 @@ public:
         int leave = dp(nums, i+1);
         return memo[i] = max(take, leave);
     }
-    int rob(vector<int>& nums) {
+    int rob1(vector<int>& nums) {
         memo.resize(nums.size(), -1);
         return dp(nums, 0);
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(), 0);
+        if(nums.size() < 2)
+            return nums[0];
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i=2;i<nums.size();i++)
+        {
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+        }
+
+        return dp[nums.size()-1];
     }
 };
