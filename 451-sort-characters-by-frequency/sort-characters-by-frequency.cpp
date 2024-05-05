c4309a59633722707a68c61c9306eb26f43e677c
deadrow <ankursingh.nit5@gmail.com>
2024-05-05 11:52:50 +0530

    Time: 13 ms (38.01%) | Memory: 11.1 MB (25.67%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>dict;
+        for(auto it : s) dict[it]++;
+
+        priority_queue<pair<int, char>, vector<pair<int,char>>>pq;
+        for(auto it : dict) pq.push({it.second, it.first});
+
+        string ret;
+        while(!pq.empty()) {
+            ret += string(pq.top().first, pq.top().second);
+            pq.pop();
+        }
+
+        return ret;
+    }
+};
