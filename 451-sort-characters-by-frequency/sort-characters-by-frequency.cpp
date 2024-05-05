ab5e0952c725cb3e5fd0513506fa0b510e92dce8
deadrow <ankursingh.nit5@gmail.com>
2024-05-05 12:42:33 +0530

    Time: 10 ms (64.70%) | Memory: 12.4 MB (10.05%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    string frequencySort(string s) {
+    // priority queue + hash map O(nlogn)
+    string frequencySort1(string s) {
         map<char,int>dict;
         for(auto& it : s) dict[it]++;
 
@@ -11,6 +12,29 @@ public:
         while(!pq.empty()) {
             ret += string(pq.top().first, pq.top().second);
             pq.pop();
+        }
+
+        return ret;
+    }
+
+    // bucket sort O(n)
+    string frequencySort(string s) {
+        map<char,int>dict;
+        for(auto& it : s) dict[it]++;
+
+        int maxFreq = max_element(dict.begin(), dict.end(), [](auto a, auto b) { return a.second < b.second;})->second;
+
+        vector<char> buckets[maxFreq+1];
+        for(auto it : dict) {
+            buckets[it.second].push_back(it.first);
+        }
+
+        string ret;
+        for(int i=maxFreq;i>=1;i--) {
+            for(auto it : buckets[i]) {
+                for(int j=0; j<i;j++)
+                    ret += it;
+            }
         }
 
         return ret;
