1bf437abcac3af3d5b78cbd40529342930698c63
deadrow <ankursingh.nit5@gmail.com>
2024-05-13 10:43:52 +0530

    Time: 31 ms (51.39%) | Memory: 22.4 MB (78.99%) - LeetSync

---
+++ 490-the-maze/the-maze.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<pair<int,int>>directions{{0,1},{0,-1},{1,0},{-1,0}};
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int m = maze.size();
+        int n = maze[0].size();
+
+        queue<pair<int,int>>bfs;
+        bfs.push({start[0], start[1]});
+
+        set<pair<int,int>>visited;
+        visited.insert({start[0], start[1]});
+
+        while(!bfs.empty()) {
+            int row = bfs.front().first;
+            int col = bfs.front().second;
+            bfs.pop();
+
+            if(row == destination[0] and col == destination[1]) return true;
+
+            for(auto& [x,y] : directions) {
+                int nRow = row + x;
+                int nCol = col + y;
+
+                while(nRow >= 0 && nRow < m && nCol >=0 && nCol < n && maze[nRow][nCol] == 0) {
+                    // keep rolling
+                    nRow += x;
+                    nCol += y;
+                }
+
+                // we hit a wall. So move one step back
+                nRow -= x;
+                nCol -= y;
+
+                if(visited.find({nRow, nCol}) == visited.end() && !maze[nRow][nCol]) {
+                    visited.insert({nRow, nCol});
+                    bfs.push({nRow, nCol});
+                }
+            }
+        }
+
+        return false;
+    }
+};
