ddd5126b4480793c151233566f2f9e893fef952c
deadrow <ankursingh.nit5@gmail.com>
2024-05-25 09:53:50 +0530

    Time: 9 ms (75.94%) | Memory: 12.8 MB (85.42%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -11,27 +11,14 @@ public:
 
 class Solution {
 public:
-    bool isChildOf(Node* p, Node * q) {
-        if(!p) return false;
-        if(p == q) return true;
-        bool child = false;
-        child = isChildOf(p->parent,q);
-        return child;
-    }
-    Node* traverse(Node* p, Node * q) {
-        if(!p or !q) return nullptr;
-        Node* parentP = traverse(p->parent,q);
-        Node* parentQ = traverse(p,q->parent);
-        if(p == q) return p;
-        if(p == parentQ) return p;
-        if(parentP == q) return q;
-        return parentP == parentQ ? parentP : nullptr;
-    }
 
     Node* lowestCommonAncestor(Node* p, Node * q) {
-        // if p is child of q
-        if(isChildOf(p,q)) return q;
-        if(isChildOf(q,p)) return p;
-        return traverse(p,q);
+       Node* a=p;
+       Node* b=q;
+       while(a != b) {
+        a = a ? a->parent : q;
+        b = b ? b->parent : p;
+       } 
+       return a;
     }
 };
