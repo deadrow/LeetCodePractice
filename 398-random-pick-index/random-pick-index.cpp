2a8024260cfe51ba843cf4359014ceb161818d1f
deadrow <ankursingh.nit5@gmail.com>
2024-05-26 11:57:44 +0530

    Time: 2173 ms (5.12%) | Memory: 47.3 MB (77.10%) - LeetSync

--- 398-random-pick-index/random-pick-index.cpp
+++ 398-random-pick-index/random-pick-index.cpp
@@ -1,7 +1,7 @@
-class Solution {
+class Solution1 {
 public:
     unordered_map<int,vector<int>>dict;
-    Solution(vector<int>& nums) {
+    Solution1(vector<int>& nums) {
         for(int i=0;i<nums.size();i++) {
             dict[nums[i]].push_back(i);
         }
@@ -10,6 +10,28 @@ public:
     int pick(int target) {
         int curSize = dict[target].size();
         return dict[target][rand()%curSize];
+    }
+};
+
+// reservoir sampling
+class Solution {
+public:
+    vector<int> nums;
+    Solution(vector<int>& nums) {
+        this->nums = nums;
+    }
+    
+    int pick(int target) {
+        int count = 0;
+        int pickIdx = 0;
+        for(int i=0;i<nums.size();i++) {
+            if(target == nums[i]) {
+                count++;
+                if(rand()%(count) == count-1)
+                    pickIdx = i;
+            }
+        }
+        return pickIdx;
     }
 };
 
