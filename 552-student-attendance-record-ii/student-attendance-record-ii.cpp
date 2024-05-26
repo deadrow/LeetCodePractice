37a4ac7e6dcad9b8ecc3c1e1fcd0e28dbf97a83b
deadrow <ankursingh.nit5@gmail.com>
2024-05-26 17:42:49 +0530

    Time: 1222 ms (22.35%) | Memory: 399.2 MB (29.81%) - LeetSync

---
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    vector<vector<vector<int>>>memo;
+    int dfs(int n, int numAbsents, int numLates) {
+        if(numAbsents >= 2 or numLates >= 3) return 0;
+        if(n == 0) return 1;
+
+        if(memo[n][numAbsents][numLates] != -1)
+            return memo[n][numAbsents][numLates];
+
+        int count = 0;
+        // select P
+        count = dfs(n-1, numAbsents, 0) % MOD;
+        //select A
+        count = (count + dfs(n-1, numAbsents+1, 0)) % MOD;
+        //select L
+        count = (count + dfs(n-1, numAbsents, numLates+1)) % MOD;
+        return memo[n][numAbsents][numLates]= count;
+    }
+    int checkRecord(int n) {
+        memo.resize(n+1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return dfs(n, 0,0);
+    }
+};
