a813e7b883727cfb35a74d528eda1bc9a0f9db66
deadrow <ankursingh.nit5@gmail.com>
2024-06-01 21:35:01 +0530

    Time: 5 ms (21.09%) | Memory: 7.5 MB (52.29%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse1(int x) {
+        // when it allows storing in 64 bit numbers
         string ret = to_string(x);
         bool negative = ret[0] == '-'? true:false;
         ::reverse(ret.begin(),ret.end());
         long retNum = stol(ret.substr(0,ret.size()));
         if(retNum < INT_MIN or retNum > INT_MAX) return 0;
         return negative ? retNum*-1 : retNum;
+    }
+
+    int reverse(int x) {
+        int ret = 0;
+        while(x) {
+            if(ret > INT_MAX/10 or ret< INT_MIN/10) return 0;
+            ret = ret*10 + x%10;
+            x /= 10;
+        }
+        return ret;
     }
 };
