122028eeb1bf952ce5aa3819a1ce284a45f5061c
deadrow <ankursingh.nit5@gmail.com>
2024-06-01 16:16:55 +0530

    Time: 437 ms (19.40%) | Memory: 25.4 MB (39.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
+    string longestPalindrome1(string s) {
         // Palindrom strings have center, left and right
         // in case of odd length and 2 centers in case
         // of event length
@@ -33,5 +33,26 @@ public:
             }
         }
         return res;
+    }
+
+    string longestPalindrome(string s) {
+        // dp solution
+        // for s = "babad" we check if smaller string is
+        // palindrome like aba and then converge to b
+        int n = s.size();
+        vector<vector<bool>>dp(n, vector<bool>(n));
+        int start = 0;
+        int end = 0;
+
+        for(int i=n-1;i>=0;i--) {
+            for(int j=i+1;j<n;j++){
+                dp[i][j] = s[i]==s[j] && (j-i<=2 or dp[i+1][j-1]);
+                if(dp[i][j] && j-i > end-start) { // if we found new string after expanding true 
+                    start = i;
+                    end = j;
+                }
+            }
+        }
+        return s.substr(start, end-start+1);
     }
 };
