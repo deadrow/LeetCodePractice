8d728371ec02fa1561b42b4b3b8072e593d6368b
deadrow <ankursingh.nit5@gmail.com>
2024-06-02 16:46:47 +0530

    Time: 1134 ms (25.76%) | Memory: 289.9 MB (25.16%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum1(vector<int>& nums) {
         // O(n2) solution. This gets TLE in leetcode for
         // one there where it is all zero but will work for
         // interviews.
         unordered_map<int,int>dict;
         set<vector<int>>ret;
-        unordered_set<int>dups;
+        unordered_set<int>dups; // just to avoid all zeros
         for(int i=0;i<nums.size();i++){
             if(dups.insert(nums[i]).second) {
                 for(int j=i+1;j<nums.size();j++) {
@@ -23,6 +23,28 @@ public:
             }
         }
 
+        return vector<vector<int>>(ret.begin(), ret.end());
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        set<vector<int>>ret;
+        for(int i=0;i<n;i++){
+            int low = i+1;
+            int high = n-1;
+            while(low<high) {
+                int sum = nums[i]+nums[low]+nums[high];
+                if(sum == 0){
+                    ret.insert({nums[i],nums[low],nums[high]});
+                    low++;
+                    high--;
+                } 
+                else if(sum < 0) low++;
+                else high--;
+            }
+        }
         return vector<vector<int>>(ret.begin(), ret.end());
     }
 };
