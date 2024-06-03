6afc3067b6e237bac6024408799fb63bcbc3f9d6
deadrow <ankursingh.nit5@gmail.com>
2024-06-03 22:34:35 +0530

    Time: 20 ms (77.81%) | Memory: 16.4 MB (61.23%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.cpp
+++ 259-3sum-smaller/3sum-smaller.cpp
@@ -11,6 +11,11 @@ public:
             while(low<high) {
                 int sum = nums[i]+nums[low]+nums[high];
                 if(sum < target) {
+                    // Logic if arr = [1,5,7,9] and target=20
+                    // if i=0, low=1 and high=3 and sum < target
+                    // it means all the elements less than high
+                    // would also be less than target. So count
+                    // them
                     count += high-low;
                     low++;
                 }
