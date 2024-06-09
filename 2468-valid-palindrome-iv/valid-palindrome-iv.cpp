d38b2231ba86fc70f112899befec7a12920f42aa
deadrow <ankursingh.nit5@gmail.com>
2024-06-09 15:54:30 +0530

    Time: 11 ms (28.90%) | Memory: 9.3 MB (49.54%) - LeetSync

--- 2468-valid-palindrome-iv/valid-palindrome-iv.cpp
+++ 2468-valid-palindrome-iv/valid-palindrome-iv.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int i, int j){
-        while(i<j) {
-            if(s[i++] != s[j--]) return false;
-        }
-        return true;
-    }
     bool makePalindrome(string s) {
         int i=0;
         int j=s.size()-1;
