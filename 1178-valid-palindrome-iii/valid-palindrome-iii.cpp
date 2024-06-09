9feaf1759e05169071bfbb67b88cad4601acd919
deadrow <ankursingh.nit5@gmail.com>
2024-06-09 12:02:27 +0530

    Time: 32 ms (18.67%) | Memory: 18.3 MB (16.36%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.cpp
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.cpp
@@ -11,8 +11,35 @@ public:
             return dict[key]=palindrome(s, i+1, j-1, k);
         return dict[key] = (palindrome(s, i+1, j, k-1) || palindrome(s, i, j-1, k-1));
     }
-    bool isValidPalindrome(string s, int k) {
+    bool isValidPalindrome1(string s, int k) {
         int n = s.size();
         return palindrome(s, 0, s.size()-1, k);
+    }
+
+    // Second approach. Find Longest common subsequence of
+    // string and reverse string. If lcs is x and x +k >= len(s)
+    // return true;
+    vector<vector<int>>memo;
+
+    // top down with memoization
+    int lcs(string& s1, string& s2, int i, int j)
+    {
+        if(i < 0 || j < 0)
+            return 0;
+        
+        if(memo[i][j] != -1)
+            return memo[i][j];
+
+        if(s1[i] == s2[j])
+            return memo[i][j] = 1 + lcs(s1, s2, i-1, j-1);
+        return memo[i][j] = max(lcs(s1, s2, i-1, j), lcs(s1, s2, i, j-1));
+    }
+
+    bool isValidPalindrome(string s, int k) {
+        string rev(s);
+        reverse(rev.begin(), rev.end());
+
+        memo.resize(s.size(), vector<int>(s.size(), -1));
+        return lcs(s, rev, s.size()-1, s.size()-1) + k >= s.size();
     }
 };
