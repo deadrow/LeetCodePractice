7b1047690dc9f37783355177651d460e58bcadea
deadrow <ankursingh.nit5@gmail.com>
2024-06-17 11:27:49 +0530

    Time: 18 ms (15.92%) | Memory: 20.4 MB (74.80%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if(intervals.empty()) return {newInterval};
-        auto it = upper_bound(intervals.begin(), intervals.end(), newInterval);
-        int idx = it-intervals.begin();
-        cout << idx;
+        // simplest solution is to push newInterval to intervals
+        // sort it and then it becomes simple merge intervals
 
-        vector<vector<int>> mergeIntervals;
-        for(int i=0;i<idx;i++)
-            mergeIntervals.push_back(intervals[i]);
-
-        if(!mergeIntervals.empty() and newInterval[0] <= mergeIntervals.back()[1]) {
-            mergeIntervals.back()[1] = max(mergeIntervals.back()[1], newInterval[1]);
-        } else {
-            mergeIntervals.push_back(newInterval);
-        }
-
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
         // merge
-        for(int i=idx;i<intervals.size();i++){
-            if(intervals[i][0] <= mergeIntervals.back()[1]) {
-                mergeIntervals.back()[1] = max(mergeIntervals.back()[1], intervals[i][1]);
-            } else
-                mergeIntervals.push_back(intervals[i]);
+        vector<vector<int>> ret;
+        for(int i=0;i<intervals.size();i++){
+            if(ret.empty() or ret.back()[1] < intervals[i][0])
+                ret.push_back(intervals[i]);
+            else
+                ret.back()[1] = max(ret.back()[1], intervals[i][1]);
         }
-
-        return mergeIntervals;
+        return ret;
     }
 };
