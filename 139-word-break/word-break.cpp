fec35e4ed8a0fb6f463c33e3557949bb5376e495
deadrow <ankursingh.nit5@gmail.com>
2024-06-18 20:39:17 +0530

    Time: 86 ms (5.11%) | Memory: 23.5 MB (9.50%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_set<string>dict;
+    unordered_map<string, bool>memo;
+    bool dp(string& s){
+        if(s.empty() || dict.count(s)) return true;
+
+        if(memo.count(s)) return memo[s];
+        for(int i=0;i<s.size();i++){
+            string first = s.substr(0, i);
+            string next = s.substr(i);
+            cout << first << "," << next << endl;
+            if(dict.count(first) && dp(next)) return true;
+        }
+        return memo[s]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto it : wordDict){
+            dict.insert(it);
+        }
+        return dp(s);
+    }
+};
