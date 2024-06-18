df21ae53ce646f1f9aa34422a51c10d98a92def8
deadrow <ankursingh.nit5@gmail.com>
2024-06-18 20:39:26 +0530

    Time: 41 ms (6.77%) | Memory: 23.5 MB (9.50%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -6,10 +6,10 @@ public:
         if(s.empty() || dict.count(s)) return true;
 
         if(memo.count(s)) return memo[s];
+        
         for(int i=0;i<s.size();i++){
             string first = s.substr(0, i);
             string next = s.substr(i);
-            cout << first << "," << next << endl;
             if(dict.count(first) && dp(next)) return true;
         }
         return memo[s]=false;
