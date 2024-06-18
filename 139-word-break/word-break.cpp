3a2f01d8758fefaa8d06029a239a2110c3f4e436
deadrow <ankursingh.nit5@gmail.com>
2024-06-18 21:13:51 +0530

    Time: 64 ms (5.11%) | Memory: 16.8 MB (35.16%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -14,10 +14,36 @@ public:
         }
         return memo[s]=false;
     }
-    bool wordBreak(string s, vector<string>& wordDict) {
+    bool wordBreak1(string s, vector<string>& wordDict) {
+        // First approach with dp where we take first half
+        // and then recursively check for second half. Once
+        // we that both are present in dict then we return true
+        // we memoized to prevent TLE
         for(auto it : wordDict){
             dict.insert(it);
         }
         return dp(s);
+    }
+
+    vector<int>memo1;
+    bool dp(string& s, int i){
+        if(i >= s.size()) return true;
+
+        if(memo1[i] != -1) return memo1[i];
+        for(int cur=i+1;cur<=s.size();cur++){
+            string first = s.substr(i, cur-i);
+            cout << first << endl;
+            if(dict.count(first) && dp(s, cur)) return memo1[i]=true;
+        }
+        return memo1[i]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Second approach is to pass the starting index
+        // of string in the recursion so that we can optimise
+        for(auto it : wordDict){
+            dict.insert(it);
+        }
+        memo1.resize(s.size()+1, -1);
+        return dp(s, 0);
     }
 };
