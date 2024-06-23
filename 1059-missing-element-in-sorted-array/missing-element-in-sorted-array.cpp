1f91f6669abfa0efe21a0a6d1dc7f76eade13250
deadrow <ankursingh.nit5@gmail.com>
2024-06-23 22:38:32 +0530

    Time: 33 ms (92.24%) | Memory: 33.8 MB (40.74%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.cpp
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
-                // LOGIC
-        // If arr = [2, 3, 4, 7, 11] and k = 5
+        // LOGIC
+        // If arr = [4,7,9,10] and k = 3
         // If we say that there is no element
         // missing the array should be
-        // [1,2,3,4,5] If we negate both array
+        // [4,5,6,7] If we negate both array
         // we find how many elements at each index is missing
-        // So the results would be [1, 1, 1, 3, 6]. Now we 
+        // So the results would be [0, 2, 3, 3]. Now we 
         // can binary search for k and we find out at which low
         // and high index between which k lies
         int low = 0;
@@ -23,10 +23,10 @@ public:
         cout << low << "," << high;
         // What to return
         // Here is the logic. For above example low and high both stops 
-        // at index 4. This is upper bound. So we take low-1
-        // At index 3 there are  elements missing. So we add k-3 that is
-        // 2 to arr[low-1] =  7 + 2 so 9 becomes our 5th missing element
-        // So arr[low-1] + (k - (arr[low-1] - low)) which becomes low+k
+        // at index 1. This is upper bound. So we take low-1
+        // At index 1 there are 2 elements missing. So the formula would be
+        // arr[low-1] + k - (arr[low-1] - (arr[0] + low - 1)) which would get
+        // simplified as k + arr[0] + low - 1;
 
         return low+k+nums[0]-1; // here is logic for this
     }
