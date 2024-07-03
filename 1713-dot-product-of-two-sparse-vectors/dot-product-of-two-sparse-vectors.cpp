7126769dafa718d1ffaa307ce1a6274c872a4cd0
deadrow <ankursingh.nit5@gmail.com>
2024-07-03 12:47:59 +0530

    Time: 128 ms (92.35%) | Memory: 168.6 MB (83.37%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.cpp
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.cpp
@@ -1,7 +1,7 @@
-class SparseVector {
+class SparseVector1 {
 public:
     unordered_map<int,int>idxToVal;
-    SparseVector(vector<int> &nums) {
+    SparseVector1(vector<int> &nums) {
         for(int i=0;i<nums.size();i++){
             if(nums[i] != 0)
                 idxToVal[i] = nums[i];
@@ -9,11 +9,37 @@ public:
     }
     
     // Return the dotProduct of two sparse vectors
-    int dotProduct(SparseVector& vec) {
+    int dotProduct(SparseVector1& vec) {
         int sum=0;
         for(auto& it : idxToVal){
             if(vec.idxToVal.count(it.first))
                 sum += it.second * vec.idxToVal[it.first];
+        }
+        return sum;
+    }
+};
+
+// 2pointers solution
+class SparseVector {
+public:
+    vector<pair<int,int>>pairs;
+    SparseVector(vector<int> &nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0)
+                pairs.push_back({i, nums[i]});
+        }
+    }
+    
+    // Return the dotProduct of two sparse vectors
+    int dotProduct(SparseVector& vec) {
+        int sum=0;
+        int i=0,j=0;
+        while(i<pairs.size() and j<vec.pairs.size()){
+            if(pairs[i].first == vec.pairs[j].first){
+                sum += pairs[i].second*vec.pairs[j].second;
+                i++,j++;
+            } else if(pairs[i].first < vec.pairs[j].first) i++;
+            else j++;
         }
         return sum;
     }
