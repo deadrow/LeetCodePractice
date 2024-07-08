64e06e098c241aa1d944bbc8582a597d0b0fc13e
deadrow <ankursingh.nit5@gmail.com>
2024-07-08 16:47:46 +0530

    Time: 37 ms (86.14%) | Memory: 18 MB (94.27%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -4,8 +4,8 @@ public:
         int k = 10;
         if(s.size() < k) return {};
 
-        long base = 26;
-//        unordered_map<char, int> to_int = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+        long base = 4;
+        unordered_map<char, int> to_int = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
         long powerk = pow(base, k-1);
         long hash = 0;
         int i=0;
@@ -13,12 +13,12 @@ public:
         unordered_set<long>dict;
         unordered_set<string>ret;
         for(int j=0;j<s.size();j++){
-            hash = hash*base + (s[j]-'A'+1);
+            hash = hash*base + (to_int[s[j]]);
             if(j-i+1 == k){
                 if(dict.count(hash))
                     ret.insert(s.substr(i, j-i+1));
                 dict.insert(hash);
-                hash = hash - (s[i]-'A'+1)*powerk;
+                hash = hash - (to_int[s[i]])*powerk;
                 
                 i++;
             }
