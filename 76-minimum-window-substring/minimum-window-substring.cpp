43fa3d0b6446fb148ca132cdfcf9ebdc096f5713
deadrow <ankursingh.nit5@gmail.com>
2024-07-08 21:49:53 +0530

    Time: 19 ms (60.19%) | Memory: 10.6 MB (53.34%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.size() < t.size()) return "";
+
+        unordered_map<char, int>remaining;
+        for(auto it : t) remaining[it]++;
+
+        int count=remaining.size();
+        int minWindow = INT_MAX;
+        int i=0,j=0;
+        int idx=-1;
+        string ret;
+        for(;j<s.size();j++){
+            if(remaining.count(s[j])){
+                 remaining[s[j]]--; // negative remain value indicates that we have more than we need
+                 if(remaining[s[j]] == 0)
+                    count--;
+            }
+
+            if(count == 0){
+                // we found one window
+                // save and find smaller
+                if(j-i+1 < minWindow){
+                    minWindow = j-i+1;
+                    idx = i;
+                }
+
+                // shrink window till count is 0
+                while(count == 0)
+                {
+                    if(j-i+1 < minWindow){
+                        minWindow = j-i+1;
+                        idx = i;
+                    }
+                    if(remaining.count(s[i])){
+                        remaining[s[i]]++;
+                        if(remaining[s[i]]> 0)
+                            count += remaining[s[i]];
+                    }
+                    i++;
+                }
+            }
+        }
+
+        return idx ==-1 ? "" : s.substr(idx, minWindow);
+    }
+};
