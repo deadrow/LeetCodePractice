7eb3a0f4dff7bd93f54bca25e4d5239d34a6e59e
deadrow <ankursingh.nit5@gmail.com>
2024-07-08 22:29:53 +0530

    Time: 21 ms (53.47%) | Memory: 10.6 MB (53.34%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -4,16 +4,15 @@ public:
         unordered_map<char, int>remaining;
         for(auto it : t) remaining[it]++;
 
-        int count=remaining.size();
+        int count=t.size();
         int minWindow = INT_MAX;
         int i=0,j=0;
         int idx=-1;
         for(;j<s.size();j++){
-            if(remaining.count(s[j])){
-                 remaining[s[j]]--; // negative remain value indicates that we have more than we need
-                 if(remaining[s[j]] == 0)
-                    count--;
-            }
+            if(remaining[s[j]] > 0)
+                count--;
+
+            remaining[s[j]]--; // chars which are present in s but not in t so it would be negative
 
             if(count == 0){
                 // shrink window till count is 0
@@ -25,11 +24,11 @@ public:
                         minWindow = j-i+1;
                         idx = i;
                     }
-                    if(remaining.count(s[i])){
-                        remaining[s[i]]++;
-                        if(remaining[s[i]]> 0)
-                            count += remaining[s[i]];
-                    }
+                    
+                    remaining[s[i]]++;
+                    if(remaining[s[i]] > 0)
+                        count++;
+                    
                     i++;
                 }
             }
