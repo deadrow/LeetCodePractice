2b484d5839d527683bc279b7638a3c1f9bd9bc00
deadrow <ankursingh.nit5@gmail.com>
2024-07-08 16:47:06 +0530

    Time: 35 ms (90.03%) | Memory: 17.9 MB (97.14%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        if(s.size() < 10) return {};
+        int k = 10;
+        if(s.size() < k) return {};
 
-        vector<string>ret;
-        unordered_map<string,int>dict;
-        int rollingHash=0;
-        for(int j=0;j<=s.size()-10;j++){
-            string cur = s.substr(j, 10);
-            dict[cur]++;
-            if(dict[cur] == 2)
-                ret.push_back(cur);
+        long base = 26;
+//        unordered_map<char, int> to_int = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+        long powerk = pow(base, k-1);
+        long hash = 0;
+        int i=0;
+        
+        unordered_set<long>dict;
+        unordered_set<string>ret;
+        for(int j=0;j<s.size();j++){
+            hash = hash*base + (s[j]-'A'+1);
+            if(j-i+1 == k){
+                if(dict.count(hash))
+                    ret.insert(s.substr(i, j-i+1));
+                dict.insert(hash);
+                hash = hash - (s[i]-'A'+1)*powerk;
+                
+                i++;
+            }
         }
-
-        return ret;
+        return vector<string>(ret.begin(), ret.end());
     }
 };
