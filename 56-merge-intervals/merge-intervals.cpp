aed0dd120dcf6581e5b55c9605de0fc33cbc1e03
deadrow <ankursingh.nit5@gmail.com>
2024-07-09 17:49:27 +0530

    Time: 27 ms (33.71%) | Memory: 25.1 MB (6.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge1(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         vector<vector<int>>ret;
         ret.push_back(intervals[0]);
@@ -12,6 +12,29 @@ public:
                 ret.push_back(intervals[i]);
         }
 
+        return ret;
+    }
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ret;
+        map<int,int>dict;
+        for(auto it : intervals){
+            dict[it[0]] += 1;
+            dict[it[1]] -= 1;
+        }
+
+        int start = -1;
+        int end = -1;
+        int count=0;
+        for(auto& [key,value] : dict){
+            if(count==0)
+                start = key;
+            count += value;
+            if(count==0){
+                end = key;
+                ret.push_back({start, end});
+            }
+        }
         return ret;
     }
 };
