df8323ef5ee36bba4a993e0e4867da48c0f9ee52
deadrow <ankursingh.nit5@gmail.com>
2024-07-10 22:03:09 +0530

    Time: 19 ms (58.68%) | Memory: 13.5 MB (56.23%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int calculate(string s) {
         stack<int>st;
-        s += '+';
-        long num =0;
+        int num =0;
         char op='+';
         for(int i=0;i<s.size();i++){
             if(isdigit(s[i])){
-                num = num*10 + s[i]-'0';
-                // continue;
+                num = num*10 + (s[i]-'0');
             }
-            else if((!isdigit(s[i]) and !isspace(s[i]))){
+            if((!isdigit(s[i]) and !isspace(s[i])) or i == s.size()-1){
                 if(op == '+'){
                     st.push(num);
                 } else if(op == '-'){
