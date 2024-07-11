0698734e14be8b263202e8ce89437750237fa385
deadrow <ankursingh.nit5@gmail.com>
2024-07-11 11:22:47 +0530

    Time: 30 ms (13.01%) | Memory: 154.4 MB (5.03%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int sum(stack<int>&st){
+        int ret=0;
+        while(!st.empty()){
+            ret += st.top();
+            st.pop();
+        }
+        return ret;
+    }
+    void evaluate(stack<int>&st, char op, int num){
+        if(op == '+'){
+            st.push(num);
+        } else if(op == '-'){
+            st.push(-num);  
+        } else if(op == '('){
+            int num1 = st.top();
+            st.pop();
+            st.push(num1*num);
+        } else if(op == '/'){
+            int num1 = st.top();
+            st.pop();
+            st.push(num1/num);
+        }
+    }
+    pair<int,int> helper(string s){
+        int num = 0;
+        char op='+';
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                num = num*10 + (s[i]-'0');
+            }
+            else if(s[i]=='+' or s[i]=='-' or s[i]=='*' or s[i]=='/'){
+                evaluate(st, op, num);
+                op = s[i];
+                num = 0;
+            } else if(s[i] == '('){
+                pair<int,int> curRes = helper(s.substr(i+1));
+                num = curRes.first;
+                i += curRes.second;
+            } else if(s[i] == ')'){
+                evaluate(st, op, num);
+                return {sum(st), i+1};
+            }
+        }
+        evaluate(st, op, num);
+        return {sum(st), s.size()};
+    }
+    int calculate(string s) {
+        return helper(s).first;
+    }
+};
