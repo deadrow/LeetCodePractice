2d1dd073a47513734c998acf1983e85c58e891ee
deadrow <ankursingh.nit5@gmail.com>
2024-07-12 20:18:58 +0530

    Time: 7 ms (98.98%) | Memory: 18 MB (26.53%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+        vector<int>indegree(numCourses);
+        vector<vector<int>>adjMatrix(numCourses); 
+        for(auto it : prerequisites){
+            indegree[it[0]]++;
+            adjMatrix[it[1]].push_back(it[0]);
+        }
+
+        queue<int>bfs;
+        vector<int>ret;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i] == 0){
+                bfs.push(i);
+            }
+        }
+        
+        while(!bfs.empty()){
+            auto cur = bfs.front();
+            bfs.pop();
+            ret.push_back(cur);
+            for(auto v : adjMatrix[cur]){
+                indegree[v]--;
+                if(indegree[v] == 0)
+                    bfs.push(v);
+            }
+        }
+        return ret.size() == numCourses;
+    }
+};
