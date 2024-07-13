e35bfa3307d4065bc355db0714392ab718754fce
deadrow <ankursingh.nit5@gmail.com>
2024-07-13 17:14:33 +0530

    Time: 39 ms (31.71%) | Memory: 23.8 MB (26.44%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<pair<int,int>>directions{{0,1},{0,-1},{1,0},{-1,0}};
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        const int INF = 2147483647;
+        int m = rooms.size();
+        int n = rooms[0].size();
+        queue<vector<int>>bfs;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(rooms[i][j] == 0)
+                    bfs.push({i,j,0});
+            }
+        }
+
+        while(!bfs.empty()){
+            int size = bfs.size();
+            for(int i=0;i<size;i++){
+                auto cur = bfs.front();
+                bfs.pop();
+                            
+                for(auto& it : directions){
+                    int nRow = it.first + cur[0];
+                    int nCol = it.second + cur[1];
+                    if(nRow>=0 and nRow<m and nCol>=0 and nCol<n and rooms[nRow][nCol] == INF){
+                        rooms[nRow][nCol] = cur[2]+1;
+                        bfs.push({nRow, nCol, cur[2]+1});
+                    }
+                }
+            }
+        }
+    }
+};
