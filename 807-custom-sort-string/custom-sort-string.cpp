c63b005a1819186f6eac7de2a900efc2760507a2
deadrow <ankursingh.nit5@gmail.com>
2024-07-13 22:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (33.52%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string customSortString(string order, string s) {
+    string customSortString1(string order, string s) {
         unordered_map<char, int>dict;
         for(int i=0;i<order.size();i++) dict[order[i]] = i;
         int counter = s.size()-1;
@@ -10,5 +10,21 @@ public:
             return idx_a < idx_b;
         });
         return s;
+    }
+
+    string customSortString(string order, string s) {
+        unordered_map<char, int>dict;
+        for(auto it : s) dict[it]++;
+        string ret;
+        for(auto it : order){
+            if(dict.count(it)){
+                ret += string(dict[it], it);
+                dict.erase(it);
+            }  
+        }
+
+        for(auto it : dict)
+            ret += string(it.second, it.first);
+        return ret;
     }
 };
