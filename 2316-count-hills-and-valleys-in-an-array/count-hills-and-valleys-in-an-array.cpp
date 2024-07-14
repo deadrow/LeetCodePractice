add03eded4806c44fb9d2301813b3e1f7462c290
deadrow <ankursingh.nit5@gmail.com>
2024-07-14 22:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (7.99%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countHillValley(vector<int>& nums) {
+    int countHillValley1(vector<int>& nums) {
         int hill = 0;
         int valley = 0;
         int prev=nums[0];
@@ -20,4 +20,20 @@ public:
         }
         return hill+valley;
     }
+    
+    // https://leetcode.com/problems/count-hills-and-valleys-in-an-array/solutions/1879182/two-pointers/
+    // Votrubac solution
+    int countHillValley(vector<int>& nums) {
+        int ret=0;
+        for(int i=1,j=0;i<nums.size()-1;i++){
+            if((nums[j] < nums[i] and nums[i] > nums[i+1]) or
+                (nums[j] > nums[i] and nums[i] < nums[i+1])
+            ) {
+                ret++;
+                j=i;
+            }
+        }
+        return ret;
+    }
+
 };
