3d062c1b057f1973fc580c79ab33b3b2f871f242
deadrow <ankursingh.nit5@gmail.com>
2024-07-15 22:18:21 +0530

    Time: 24 ms (88.02%) | Memory: 30.5 MB (19.16%) - LeetSync

--- 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.cpp
+++ 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.cpp
@@ -30,7 +30,7 @@ public:
         TreeNode* lcaNode = lca(root, p, q);
         return distance(lcaNode, p) + distance(lcaNode, q);
     }
-    int findDistance(TreeNode* root, int p, int q) {
+    int findDistance2(TreeNode* root, int p, int q) {
         TreeNode* lcaNode = lca(root, p, q);
         queue<pair<TreeNode*,int>>bfs;
         bfs.push({lcaNode,0});
@@ -58,5 +58,29 @@ public:
                 bfs.push({cur.first->right, cur.second+1});
         }
         return totalDepth;
+    }
+    int distance(TreeNode* root, int p, int q, int& pDist, int& qDist, int dist){
+        if(!root) return 0;
+        int left = distance(root->left, p, q, pDist, qDist, dist+1);
+        int right = distance(root->right, p, q, pDist, qDist, dist+1);
+        
+        if(root->val == p){
+            pDist = dist;
+            return dist;
+        }
+        if(root->val == q){
+            qDist = dist;
+            return dist;
+        }
+        
+        if(left and right) return dist;
+        return left ? left : right;
+
+    }
+    int findDistance(TreeNode* root, int p, int q) {
+        int pDist=-1, qDist=-1;
+        int dist = distance(root, p, q, pDist, qDist, 0);
+        if(pDist == -1 or qDist == -1) return 0;
+        return pDist + qDist - 2*dist;
     }
 };
