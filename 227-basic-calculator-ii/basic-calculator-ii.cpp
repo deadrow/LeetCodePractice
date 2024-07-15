8b3a3fd38c67b057d170e1fe9abd5b9b4355bceb
deadrow <ankursingh.nit5@gmail.com>
2024-07-15 11:15:57 +0530

    Time: 21 ms (47.57%) | Memory: 15.7 MB (11.69%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,66 +1,53 @@
 class Solution {
 public:
-    int calculate1(string s) {
-        stack<int>st;
-        int num =0;
-        char op='+';
-        for(int i=0;i<s.size();i++){
-            if(isdigit(s[i])){
-                num = num*10 + (s[i]-'0');
-            }
-            if((!isdigit(s[i]) and !isspace(s[i])) or i == s.size()-1){
-                if(op == '+'){
-                    st.push(num);
-                } else if(op == '-'){
-                    st.push(-num);  
-                } else if(op == '*'){
-                    int num1 = st.top();
-                    st.pop();
-                    st.push(num1*num);
-                } else if(op == '/'){
-                    int num1 = st.top();
-                    st.pop();
-                    st.push(num1/num);
-                }
-                op = s[i];
-                num = 0;
-            }
-        }
-
-        int ret=0;
+    int sum(stack<int>&st){
+        int sum=0;
         while(!st.empty()){
-            ret += st.top();
+            sum += st.top();
             st.pop();
         }
-        return ret;
+        return sum;
     }
-    
-    int calculate(string s) {
-        int prev=0;
-        int num=0;
+    void evaulate(stack<int>&st, char op, int num){
+        if(op == '+')
+            st.push(num);
+        else if(op == '-')
+            st.push(-num);
+        else if(op=='*'){
+            int num1= st.top();
+            st.pop();
+            st.push(num1*num);
+        } else if(op=='/'){
+            int num1= st.top();
+            st.pop();
+            st.push(num1/num);
+        }
+    }
+    pair<int,int> helper(string s){
+        long num=0;
         char op='+';
-        int ret=0;
+        stack<int>st;
         for(int i=0;i<s.size();i++){
-            if(isdigit(s[i])){
-                num = num*10 + (s[i]-'0');
-            }
-            if((!isdigit(s[i]) and !isspace(s[i])) or i == s.size()-1){
-                if(op == '+'){
-                    ret += prev;
-                    prev = num;
-                } else if(op == '-'){
-                    ret += prev;
-                    prev = -num;  
-                } else if(op == '*'){
-                    prev = prev*num;
-                } else if(op == '/'){
-                    prev = prev/num;
-                }
+            if(isdigit(s[i]))
+                num = num*10 + s[i]-'0';
+            else if(s[i]== '+' or s[i]=='-' or s[i]=='*' or s[i]=='/'){
+                evaulate(st, op, num);
                 op = s[i];
                 num = 0;
+            } else if(s[i] == '('){
+                auto ret = helper(s.substr(i+1));
+                num += ret.first;
+                i += ret.second;
+            } else if(s[i] == ')'){
+                evaulate(st, op, num);
+                return {sum(st), i+1};
             }
+                
         }
-
-        return ret+prev;
+        evaulate(st, op, num);
+        return {sum(st), s.size()};
+    }
+    int calculate(string s) {
+        return helper(s).first;
     }
 };
