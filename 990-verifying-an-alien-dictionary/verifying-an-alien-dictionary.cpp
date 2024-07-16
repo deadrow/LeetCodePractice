5b977413a043fb0e7448624e92a80734897cf3f5
deadrow <ankursingh.nit5@gmail.com>
2024-07-16 16:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (72.85%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int>orderMap(26);
+        for(int i=0;i<order.size();i++) orderMap[order[i]-'a']=i;
+
+        for(int i=0;i<words.size()-1;i++){
+            for(int j=0;j<words[i].size();j++){
+                if(j >= words[i+1].size()) return false;
+                if(words[i][j] != words[i+1][j]){
+                    if(orderMap[words[i][j]-'a'] > orderMap[words[i+1][j]-'a']) return false;
+                    else break; // order is fine
+                }
+            }
+        }
+        return true;
+    }
+};
