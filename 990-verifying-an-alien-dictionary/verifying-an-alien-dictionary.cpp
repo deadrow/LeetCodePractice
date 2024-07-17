aefcafd56d4436f65cc3e7a3f1a5f8f3f8d0916f
deadrow <ankursingh.nit5@gmail.com>
2024-07-17 16:04:59 +0530

    Time: 9 ms (9.61%) | Memory: 12.7 MB (27.27%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char,int>orderMap;
-        for(int i=0;i<order.size();i++) orderMap[order[i]]=i;
+        unordered_map<char, int>mp;
+        for(int i=0;i<order.size();i++) mp[order[i]]=i;
 
         for(int i=0;i<words.size()-1;i++){
             for(int j=0;j<words[i].size();j++){
-                if(j >= words[i+1].size()) return false;
+                if(j>= words[i+1].size()) return false;
                 if(words[i][j] != words[i+1][j]){
-                    if(orderMap[words[i][j]] > orderMap[words[i+1][j]]) return false;
-                    else break; // order is fine
+                    if(mp[words[i][j]] > mp[words[i+1][j]])
+                        return false;
+                    else break;
                 }
             }
         }
