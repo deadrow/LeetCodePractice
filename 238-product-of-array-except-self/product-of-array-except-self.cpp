dbe02d5523cdaea2419a2c87caefc8b5805edd5f
deadrow <ankursingh.nit5@gmail.com>
2024-07-17 10:01:30 +0530

    Time: 33 ms (21.72%) | Memory: 40.2 MB (14.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>left(n);
+        left[0]=1;
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1]*nums[i-1];
+        }
+        vector<int>right(n);
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i]= right[i+1]*nums[i+1];
+        }
+        vector<int>ret(n);
+        for(int i=0;i<n;i++){
+            ret[i]= left[i]*right[i];
+        }
+        return ret;
+    }
+
+    vector<int> productExceptSelf1(vector<int>& nums) {
         vector<int>ret(nums.size());
         ret[0]=1;
         for(int i=1;i<nums.size();i++){
