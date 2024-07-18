cc753d128c7c31ffcce3b75d9df0f8ed038f72bc
deadrow <ankursingh.nit5@gmail.com>
2024-07-18 10:45:31 +0530

    Time: 5 ms (51.64%) | Memory: 8.4 MB (53.71%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
+        int len1 = num1.size();
+        int len2 = num2.size();
+        int i=len1-1,j=len2-1;
+        int carry=0;
         string ret;
-        int len1 = num1.size()-1;
-        int len2 = num2.size()-1;
-        int carry = 0;
-        // 11  and 321
-        while(len1 >= 0 || len2 >= 0) {
-            int n1 = len1 >=0 ? num1[len1]-'0' : 0;
-            int n2 = len2 >=0 ? num2[len2]-'0' : 0;
-            ret += ((n1 + n2 + carry) % 10 + '0');
-            carry = (n1 + n2 + carry)/10;
-            len1--;len2--;
+        while(i>=0 or j>=0){
+            int a = i<0 ? 0 : num1[i]-'0';
+            int b = j<0 ? 0 : num2[j]-'0';
+            ret += (a+b+carry)%10 + '0';
+            carry = (a+b+carry)/10;
+            i--,j--;
         }
 
-        if(carry)
-            ret += '1';
-
+        if(carry) ret += '1';
         reverse(ret.begin(), ret.end());
         return ret;
     }
