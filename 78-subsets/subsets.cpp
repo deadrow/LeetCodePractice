ed3302b2142b28871ce97422eda99fe9c853d06a
deadrow <ankursingh.nit5@gmail.com>
2024-07-18 16:13:14 +0530

    Time: 4 ms (37.83%) | Memory: 8.7 MB (40.63%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
+    // bitmasking solution
+    vector<vector<int>> subsets1(vector<int>& nums) {
         vector<vector<int>> ret;
         for(int i= pow(2, nums.size())-1;i>=0;i--){
             vector<int> cur;
@@ -9,6 +10,27 @@ public:
                     cur.push_back(nums[j]);
             }
             ret.push_back(cur);
+        }
+        return ret;
+    }
+
+    vector<vector<int>> ret;
+    void backtrack(vector<int>& nums, int cur_i, int k, vector<int>&cur){
+        if(cur.size() > k) return;
+        if(cur.size()==k) {
+            ret.push_back(cur);
+            return;
+        }
+        for(int i=cur_i;i<nums.size();i++){
+            cur.push_back(nums[i]);
+            backtrack(nums, i+1, k, cur);
+            cur.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        for(int i=0;i<=nums.size();i++){
+            vector<int>cur;
+            backtrack(nums, 0, i, cur);
         }
         return ret;
     }
