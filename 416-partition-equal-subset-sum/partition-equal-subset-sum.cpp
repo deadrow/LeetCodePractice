fbdc2a1f6c1bc1f8053440ae1db988443212304e
deadrow <ankursingh.nit5@gmail.com>
2024-07-19 22:51:33 +0530

    Time: 159 ms (52.17%) | Memory: 99.5 MB (19.50%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<vector<int>>memo;
-    bool dp(vector<int>& nums, int cur_i, int target){
-        if(target < 0) return false;
+    bool dp(vector<int>& nums, int i, int target){
+        if(i>=nums.size()) return false;
         if(target == 0) return true;
-        if(memo[cur_i][target] != -1) return memo[cur_i][target];
-        for(int i=cur_i;i<nums.size();i++){
-            if(dp(nums, i+1, target-nums[i])) return memo[cur_i][target]=true;
-        }
-        return memo[cur_i][target]=false;
+        if(memo[i][target] != -1) return memo[i][target];
+        bool take = false;
+        if(target-nums[i] >= 0)
+            take = dp(nums, i+1, target-nums[i]);
+            
+        bool leave = dp(nums,i+1, target);
+        return memo[i][target] = take or leave;
     }
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(),0);
