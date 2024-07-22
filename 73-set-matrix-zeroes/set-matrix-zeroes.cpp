96f675587a47585ae5f97220fc12a63372048169
deadrow <ankursingh.nit5@gmail.com>
2024-07-22 09:58:17 +0530

    Time: 13 ms (21.55%) | Memory: 16.8 MB (30.17%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+    void setZeroes1(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         vector<int>rows, cols;
@@ -23,6 +23,51 @@ public:
         for(auto it : cols){
             for(int i=0;i<m;i++)
                 matrix[i][it]=0;
+        }
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool isRow = false, isCol=false;
+
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0)
+                isRow=true;
+        }
+
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0)
+                isCol=true;
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]== 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]== 0 or matrix[i][0]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(isRow){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(isCol){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
         }
     }
 };
