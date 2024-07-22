99e3f7f756f8c178484e9f9492d977dccb796f7a
deadrow <ankursingh.nit5@gmail.com>
2024-07-22 14:21:10 +0530

    Time: 77 ms (65.71%) | Memory: 70.8 MB (54.29%) - LeetSync

--- 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.cpp
+++ 1893-maximum-subarray-sum-after-one-operation/maximum-subarray-sum-after-one-operation.cpp
@@ -5,6 +5,11 @@ public:
         int squaredSum=nums[0]*nums[0];
         int maxSum=max(curSum, squaredSum);
         for(int i=1;i<nums.size();i++){
+            // LOGIC
+            // sqauredSum would be max of
+            // 1. Cursum + square of current num
+            // 2. Only squared sum of current num
+            // 3. Squared sum + current num
             squaredSum = max({curSum + nums[i]*nums[i], nums[i]*nums[i], squaredSum+nums[i]});
             curSum = max(curSum+nums[i], nums[i]);
             maxSum = max({maxSum, squaredSum, curSum});
