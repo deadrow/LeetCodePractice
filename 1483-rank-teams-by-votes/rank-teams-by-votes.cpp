80fdfda8ffcea399f1537d655a307a1b8d98fd4f
deadrow <ankursingh.nit5@gmail.com>
2024-07-22 22:17:17 +0530

    Time: 14 ms (47.16%) | Memory: 14.4 MB (56.21%) - LeetSync

--- 1483-rank-teams-by-votes/rank-teams-by-votes.cpp
+++ 1483-rank-teams-by-votes/rank-teams-by-votes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string rankTeams(vector<string>& votes) {
+        // Main logic is to compare 2 vectors
+        // For e.g {5,0,0} is > than {2,3,3}
         unordered_map<char, vector<int>>mp;
         for(auto& c : votes[0])
             mp[c] = vector<int>(votes[0].size());
