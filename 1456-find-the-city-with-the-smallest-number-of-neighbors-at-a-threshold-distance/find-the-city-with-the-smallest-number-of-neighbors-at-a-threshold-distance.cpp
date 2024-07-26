0d33a45e93e78e8fc1f1315e748a21355a1f7591
deadrow <ankursingh.nit5@gmail.com>
2024-07-26 11:37:05 +0530

    Time: 10 ms (100.00%) | Memory: 15.6 MB (75.48%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+    // Dijiktra 
+    int findTheCity1(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<pair<int,int>>>adj(n);
         for(auto& it : edges){
             adj[it[0]].push_back({it[1], it[2]});
@@ -30,6 +31,41 @@ public:
                     count++;
             }
             
+            if(count <= minCount){
+                minCount=count;
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    // floyd warshall
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        int INF = 1e9+7;
+        vector<vector<int>>dist(n, vector<int>(n, INF));
+        for(int i=0;i<n;i++) dist[i][i]=0;
+
+        for(const auto& it : edges){
+            dist[it[0]][it[1]]=it[2];
+            dist[it[1]][it[0]]=it[2];
+        }
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        int minCount=n;
+        int idx=0;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(i!=j and dist[i][j] <= distanceThreshold) count++;
+            }
             if(count <= minCount){
                 minCount=count;
                 idx = i;
