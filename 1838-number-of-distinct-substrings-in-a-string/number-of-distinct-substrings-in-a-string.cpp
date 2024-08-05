69c12910ff535f3d5d914551a6b55c641c062d29
deadrow <ankursingh.nit5@gmail.com>
2024-08-05 22:40:54 +0530

    Time: 1840 ms (26.47%) | Memory: 522.8 MB (20.59%) - LeetSync

--- 1838-number-of-distinct-substrings-in-a-string/number-of-distinct-substrings-in-a-string.cpp
+++ 1838-number-of-distinct-substrings-in-a-string/number-of-distinct-substrings-in-a-string.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int countDistinct(string s) {
         unordered_set<string>mp;
-        for(int i=0;i<=s.size();i++){
-            for(int j=i+1;j<=s.size();j++){
-                mp.insert(s.substr(i,j-i));
+        for(int i=0;i<s.size();i++){
+            string st(1,s[i]);
+            mp.insert(st);
+            for(int j=i+1;j<s.size();j++){
+                st += s[j];
+                mp.insert(st);
             }
         }
         return mp.size();
