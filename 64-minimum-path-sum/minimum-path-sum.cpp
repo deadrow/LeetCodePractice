6f45785cc41d2981f7cd43fbe7b9fa7eea89f406
deadrow <ankursingh.nit5@gmail.com>
2024-08-09 22:41:17 +0530

    Time: 4 ms (77.35%) | Memory: 13 MB (26.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    vector<pair<int,int>>directions{{0,1},{1,0}};
     vector<vector<int>>memo;
     int dp(vector<vector<int>>& grid, int row, int col){
         int m = grid.size();
         int n = grid[0].size();
-        if(row==m-1 and col==n-1) return grid[m-1][n-1];
+        if(row>=m or col>=n) return INT_MAX;
+        if(row==m-1 and col==n-1) return grid[row][col];
         if(memo[row][col] != -1) return memo[row][col];
-        int res=INT_MAX;
-        for(auto& it : directions){
-            int nRow = it.first + row;
-            int nCol = it.second + col;
-            if(row>=0 and row < m and col>=0 and col<n){
-                int cur = dp(grid, nRow, nCol);
-                if(cur != INT_MAX)
-                    res = min(res, grid[row][col] + cur);
-            }
-        }
-        return memo[row][col]=res;
+        return memo[row][col]= grid[row][col] + min(dp(grid, row+1, col), dp(grid, row, col+1));
     }
     int minPathSum(vector<vector<int>>& grid) {
         memo.resize(grid.size()+1, vector<int>(grid[0].size()+1, -1));
