e5b4aa83dbbc7729ca9621fed8cbf54abdc7f017
deadrow <ankursingh.nit5@gmail.com>
2024-08-10 17:22:14 +0530

    Time: 20 ms (26.87%) | Memory: 157.4 MB (7.03%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int INF = 1e5;
+    vector<vector<int>>memo;
     int dp(int n, int curLetters, int lastCopied){
         if(curLetters > n) return INF;
         if(curLetters == n) return 0;
+        if(memo[curLetters][lastCopied] != -1) return memo[curLetters][lastCopied];
         int copy = 2 + dp(n, curLetters+curLetters, curLetters);
         int paste = 1 + dp(n, curLetters+lastCopied, lastCopied);
-        return min(copy, paste);
+        return memo[curLetters][lastCopied] = min(copy, paste);
     }
     int minSteps(int n) {
         if(n == 1) return 0;
+        memo.resize(n+1, vector<int>(n+1, -1));
         return dp(n, 1, 1)+1;
     }
 };
