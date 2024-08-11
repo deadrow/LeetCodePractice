4d23d65159ada9acdb8c7c2111b6f0c490adacaa
deadrow <ankursingh.nit5@gmail.com>
2024-08-11 22:54:30 +0530

    Time: 5 ms (36.39%) | Memory: 10.6 MB (5.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -4,15 +4,10 @@ public:
     int dp(vector<vector<int>>& obstacleGrid, int row, int col){
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if(row >= m or col>= n) return 0;
+        if(row >= m or col>= n or obstacleGrid[row][col]) return 0;
         if(row == m-1 and col == n-1) return 1;
         if(memo[row][col] != -1) return memo[row][col];
-        int sum = 0;
-        if(col+1 < n and !obstacleGrid[row][col+1])
-            sum += dp(obstacleGrid, row, col+1);
-        if(row+1 < m and !obstacleGrid[row+1][col])
-            sum += dp(obstacleGrid, row+1, col);
-        return memo[row][col]=sum;
+        return memo[row][col]=dp(obstacleGrid, row, col+1) +dp(obstacleGrid, row+1, col);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
