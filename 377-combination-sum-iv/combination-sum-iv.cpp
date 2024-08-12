24cd6303e7df7439e6ea3d7270c55a82f74c6f63
deadrow <ankursingh.nit5@gmail.com>
2024-08-12 17:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (38.09%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> ret;
     vector<int>memo;
     int dp(vector<int>& nums, int target){
         if(target == 0) return 1;
-
+        if(target < 0) return 0;
         if(memo[target] != -1) return memo[target];
-        int ways=0;
-        for(auto it : nums) {
-            if(target-it>=0)
-                ways += dp(nums, target-it);
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            count += dp(nums, target-nums[i]);
         }
-        return memo[target]=ways;
+        return memo[target]=count;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        memo.resize(target+1,-1);
+        memo.resize(target+1, -1);
         return dp(nums, target);
     }
 };
