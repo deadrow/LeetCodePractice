c8c4991c3405a1365240a7ad2b1e560025dde71f
deadrow <ankursingh.nit5@gmail.com>
2024-08-12 18:21:29 +0530

    Time: 219 ms (37.53%) | Memory: 115.2 MB (15.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<vector<int>>memo;
-    bool dp(vector<int>& nums, int i, int target){
-        if(i>=nums.size()) return false;
+    bool dp(vector<int>& nums, int target, int i){
         if(target == 0) return true;
-        if(memo[i][target] != -1) return memo[i][target];
-        bool take = false;
-        if(target-nums[i] >= 0)
-            take = dp(nums, i+1, target-nums[i]);
-            
-        bool leave = dp(nums,i+1, target);
-        return memo[i][target] = take or leave;
+        if(target < 0 or i>= nums.size()) return false;
+        if(memo[target][i] != -1) return memo[target][i];
+        bool take = dp(nums, target-nums[i], i+1);
+        bool leave = dp(nums, target, i+1);
+        return memo[target][i] = take or leave;
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(),0);
-        if(sum % 2 != 0) return false;
-        memo.resize(nums.size()+1, vector<int>(sum/2+1, -1));
-        return dp(nums, 0, sum/2);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if(total & 1) return false;
+        memo.resize(total/2+1, vector<int>(nums.size()+1, -1));
+        return dp(nums, total/2, 0);
     }
 };
