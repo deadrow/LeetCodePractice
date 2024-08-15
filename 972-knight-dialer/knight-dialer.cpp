b3048ca73a8aefc8be3dc5fc78806d5af6407ffd
deadrow <ankursingh.nit5@gmail.com>
2024-08-15 12:29:18 +0530

    Time: 166 ms (22.91%) | Memory: 31.3 MB (49.01%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
-    vector<vector<int>> jumps = {
-            {4, 6},
-            {6, 8},
-            {7, 9},
-            {4, 8},
-            {3, 9, 0},
-            {},
-            {1, 7, 0},
-            {2, 6},
-            {1, 3},
-            {2, 4}
-        };
-    vector<vector<int>>memo;
-    int dfs(int n, int cur_i)
-    {
-        if(n == 0)
-            return 1;
-
-        if(memo[n][cur_i] != -1)
-            return memo[n][cur_i];
-
-        int sum = 0;
-        for(const auto& it : jumps[cur_i])
-        {
-            sum = (sum + dfs(n-1, it)) % MOD;
+    int MOD = 1e9+7;
+    vector<vector<vector<int>>>memo;
+    vector<pair<int,int>>directions{{2,-1},{2,1},{-2,-1},{-2,1},{1,2},{1,-2},{-1,2},{-1,-2}};
+    int dp(vector<vector<int>>& grid, int row, int col, int n){
+        if(row <0 or row>=grid.size() or col<0 or col>=grid[0].size() or !grid[row][col]) return 0;
+        if(n==0) return 1;
+        if(memo[row][col][n] != -1) return memo[row][col][n];
+        int count=0;
+        for(const auto& it : directions){
+            int nRow = row + it.first;
+            int nCol = col + it.second;
+            count = (count + dp(grid, nRow, nCol, n-1)) % MOD;
         }
-
-        memo[n][cur_i] = sum;
-        return sum;
+        return memo[row][col][n]=count;
     }
-
     int knightDialer(int n) {
-        int count = 0;
-        vector<int>nums{0,1,2,3,4,6,7,8,9};
-        memo.resize(n+1, vector<int>(10,-1));
-
-        for(int i=0;i<10;i++)
-        {
-            count = (count + dfs(n-1, i)) % MOD;
+        vector<vector<int>>grid{{1,1,1},{1,1,1},{1,1,1},{0,1,0}};
+        int count=0;
+        memo.resize(4, vector<vector<int>>(3, vector<int>(n+1, -1)));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]){
+                    count = (count + dp(grid, i, j, n-1)) % MOD;
+                }
+            }
         }
-            
         return count;
     }
 };
