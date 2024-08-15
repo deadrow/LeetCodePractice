9fc3ed1e754677b772f342f2d69d5081c83b9a30
deadrow <ankursingh.nit5@gmail.com>
2024-08-15 11:43:25 +0530

    Time: 11 ms (22.93%) | Memory: 11.3 MB (23.70%) - LeetSync

---
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<pair<int,int>>directions{{0,1},{0,-1},{1,0},{-1,0}};
+    vector<vector<vector<int>>>memo;
+    int MOD = 1e9+7;
+    int dp(int m, int n, int maxMove, int row, int col){
+        if(maxMove < 0) return 0;
+        if(row>=m or row<0 or col >=n or col<0) return 1;
+        if(memo[row][col][maxMove]!= -1) return memo[row][col][maxMove];
+        int count=0;
+        for(auto& it : directions){
+            count = (count + dp(m, n, maxMove-1, row+it.first, col+it.second))%MOD;
+        }
+        return memo[row][col][maxMove] = count;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memo.resize(m+1, vector<vector<int>>(n+1, vector<int>(maxMove+1, -1)));
+        return dp(m, n, maxMove, startRow, startColumn);
+    }
+};
