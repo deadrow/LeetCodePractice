7bda338dc5ee04163c940e145bf0c4901da7354c
deadrow <ankursingh.nit5@gmail.com>
2024-08-17 11:39:15 +0530

    Time: 341 ms (89.94%) | Memory: 168.6 MB (84.22%) - LeetSync

---
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.cpp
@@ -0,0 +1,42 @@
+class StockPrice {
+public:
+    unordered_map<int,int>timestamps;
+    int latestTime=0;
+    priority_queue<pair<int,int>>maxHeap;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>minHeap;
+    StockPrice() {
+        
+    }
+    
+    void update(int timestamp, int price) {
+        latestTime = max(latestTime, timestamp);
+        timestamps[timestamp]=price;
+        maxHeap.push({price, timestamp});
+        minHeap.push({price, timestamp});
+    }
+    
+    int current() {
+        return timestamps[latestTime];
+    }
+    
+    int maximum() {
+        while(!maxHeap.empty() and timestamps[maxHeap.top().second] != maxHeap.top().first)
+            maxHeap.pop();
+        return maxHeap.top().first;
+    }
+    
+    int minimum() {
+        while(!minHeap.empty() and timestamps[minHeap.top().second] != minHeap.top().first)
+            minHeap.pop();
+        return minHeap.top().first;
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
