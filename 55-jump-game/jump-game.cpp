634cbdc1595d595db18cf8c78be31ad97bd5c6f0
deadrow <ankursingh.nit5@gmail.com>
2024-08-24 10:36:16 +0530

    Time: 37 ms (86.42%) | Memory: 51.2 MB (36.65%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        for(int i=nums.size()-2;i>=0;i--) {
-            if(i+nums[i] < i+1) {
-                // check if it is reachble from anywhere else
-                int j = i;
-                while(j>=0 and j+nums[j] <= i) j--;
-                cout << j;
-                if(j < 0) return false;
-            }
+        int n = nums.size();
+        int reachableIdx = n-1;
+        for(int i=n-2;i>=0;i--) {
+            if(i+nums[i] >= reachableIdx)
+                reachableIdx = i;
         }
-        return true;
+        return reachableIdx == 0;
     }
 };
