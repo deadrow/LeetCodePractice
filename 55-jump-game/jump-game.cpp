a315bb950b1e302b3a54a94adf120d97a3a247f2
deadrow <ankursingh.nit5@gmail.com>
2024-08-24 10:49:10 +0530

    Time: 700 ms (6.00%) | Memory: 54.1 MB (5.05%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    // Greedy
+    bool canJump1(vector<int>& nums) {
         int n = nums.size();
         int reachableIdx = n-1;
         for(int i=n-2;i>=0;i--) {
@@ -8,5 +9,18 @@ public:
                 reachableIdx = i;
         }
         return reachableIdx == 0;
+    }
+    // dp with memo
+    vector<int>memo;
+    bool dp(vector<int>& nums, int start){
+        if(start>=nums.size()-1) return true;
+        if(memo[start] != -1) return memo[start];
+        for(int i=start+1;i<=start+nums[start];i++)
+            if(dp(nums, i)) return memo[start]=true;
+        return memo[start]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        memo.resize(nums.size()+1, -1);
+        return dp(nums, 0);
     }
 };
