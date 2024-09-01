199895c7d737749b6af257ce9f2f9e8f71972207
deadrow <ankursingh.nit5@gmail.com>
2024-09-01 17:40:15 +0530

    Time: 11 ms (76.25%) | Memory: 21.3 MB (68.37%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n= citations.size();
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(citations[i] >= n-i) j++;
+        int low=0,high=n;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(citations[mid] >= n-mid)
+                high=mid;
+            else low=mid+1;
         }
-        return j;
+        return n-low;
     }
 };
