2c5f0a4081523cbdd285ff872a89170da9927882
deadrow <ankursingh.nit5@gmail.com>
2024-09-01 16:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (63.62%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end(), greater<int>());
+        // sort(citations.begin(), citations.end(), greater<int>());
+        sort(citations.begin(), citations.end());
         int n = citations.size();
         // 6 5 3 1 0
         // 1 2 3 4 5
-        for(int i=n-1;i>=0;i--){
-            if(citations[i] >= i+1) return i+1;
+        // 0 1 3 5 6
+        // 2 2 2 3 5
+        // for(int i=n-1;i>=0;i--){
+        //     if(citations[i] >= i+1) return i+1;
+        // }
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(citations[i] >= n-i) j++;
         }
 
-        return 0;
+        return j;
     }
 };
