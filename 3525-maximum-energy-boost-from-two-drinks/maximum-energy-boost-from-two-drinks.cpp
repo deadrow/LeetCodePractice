48fc39451d039a038ea9ca6ac0bbfe2c6ea1e24b
deadrow <ankursingh.nit5@gmail.com>
2024-09-02 22:22:05 +0530

    Time: 372 ms (16.93%) | Memory: 265.6 MB (24.61%) - LeetSync

--- 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
@@ -4,14 +4,8 @@ public:
     long long dp(vector<int>& energyDrinkA, vector<int>& energyDrinkB, bool fromA, int i){
         if(i>=energyDrinkA.size()) return 0;
         if(memo[i][fromA] != -1) return memo[i][fromA];
-        long long take=0,change=0;
-        if(fromA){
-            take = energyDrinkA[i] + dp(energyDrinkA, energyDrinkB, fromA, i+1);
-            change = energyDrinkA[i] + dp(energyDrinkA, energyDrinkB, !fromA, i+2);
-        } else {
-            take = energyDrinkB[i] + dp(energyDrinkA, energyDrinkB, fromA, i+1);
-            change = energyDrinkB[i] + dp(energyDrinkA, energyDrinkB, !fromA, i+2);
-        }
+        long long take = (fromA ? energyDrinkA[i] : energyDrinkB[i]) + dp(energyDrinkA, energyDrinkB, fromA, i+1);
+        long long change = (fromA ? energyDrinkA[i] : energyDrinkB[i]) + dp(energyDrinkA, energyDrinkB, !fromA, i+2);
         return memo[i][fromA]=max(take, change);
     }
     long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
