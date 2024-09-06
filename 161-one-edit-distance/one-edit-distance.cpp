2872a336d7e7ae6b62d724ad66d92252859a82fe
deadrow <ankursingh.nit5@gmail.com>
2024-09-06 12:07:14 +0530

    Time: 5 ms (22.48%) | Memory: 8.4 MB (23.95%) - LeetSync

--- 161-one-edit-distance/one-edit-distance.cpp
+++ 161-one-edit-distance/one-edit-distance.cpp
@@ -7,7 +7,7 @@ public:
             if(j==t.size()) return s.substr(i).size() == 1;
             if(s[i]==t[j]){
                 i++,j++;
-            } else if(s[i]!=t[j]) {
+            } else {
                 if((s.substr(i+1) == t.substr(j)) or (s.substr(i) == t.substr(j+1)) or s.substr(i+1)==t.substr(j+1))
                     return true;
                 else return false;
