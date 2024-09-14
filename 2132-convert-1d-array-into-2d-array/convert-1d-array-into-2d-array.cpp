278cab4c16a15deaa64e5de4ff61552e24c2d739
deadrow <ankursingh.nit5@gmail.com>
2024-09-14 21:40:30 +0530

    Time: 84 ms (43.67%) | Memory: 88.1 MB (75.77%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -3,14 +3,8 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(m*n != original.size()) return {};
         vector<vector<int>> ret(m, vector<int>(n));
-        int j=0,k=0;
         for(int i=0;i<original.size();i++){
-            if(k==n){
-                j++;
-                k=0;
-            }
-            ret[j][k] = original[i];
-            k++;
+            ret[i/n][i%n] = original[i];
         }
         return ret;
     }
