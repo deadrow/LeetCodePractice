b195be7cc95a96d4518dccc94c393de27deef9b2
deadrow <ankursingh.nit5@gmail.com>
2024-09-15 10:48:18 +0530

    Time: 217 ms (33.33%) | Memory: 180.3 MB (33.33%) - LeetSync

--- 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     vector<vector<long long>>memo;
     long long dp(vector<int>& a, vector<int>& b, int i, int j){
-        if(j>=b.size() and i< a.size()) return INT_MIN;
         if(i>=a.size()) return 0;
+        if(j>=b.size()) return INT_MIN;
         if(memo[i][j] != -1) return memo[i][j];
-        long long take = INT_MIN;
-        long long ret = dp(a, b, i+1, j+1);
-        if(ret != INT_MIN)
-            take = (long long)b[j]*a[i] + ret;
+        long long take = (long long)b[j]*a[i] + dp(a, b, i+1, j+1);
         long long leave = dp(a, b, i, j+1);
         return memo[i][j]=max(take, leave);
     }
